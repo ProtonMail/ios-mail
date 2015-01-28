6bb7cf88a795362a0a2bb83831565c389f7460e4
Eric Chamberlain <eric.chamberlain@arctouch.com>
2015-01-27 17:42:14 -0800

    PRO-26 - Mailbox password screen
    Added MailboxPassword screen and view controller.
    
    Update other common UI components.

---
+++ ProtonMail/ProtonMail/MailboxPasswordViewController.swift
@@ -0,0 +1,49 @@
+//
+//  MailboxPasswordViewController.swift
+//  ProtonMail
+//
+//
+// Copyright 2015 ArcTouch, Inc.
+// All rights reserved.
+//
+// This file, its contents, concepts, methods, behavior, and operation
+// (collectively the "Software") are protected by trade secret, patent,
+// and copyright laws. The use of the Software is governed by a license
+// agreement. Disclosure of the Software to third parties, in any form,
+// in whole or in part, is expressly prohibited except as authorized by
+// the license agreement.
+//
+
+import Foundation
+
+class MailboxPasswordViewController: UIViewController {
+    @IBOutlet weak var decryptButton: UIButton!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var rememberButton: UIButton!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        decryptButton.roundCorners()
+        passwordTextField.roundCorners()
+    }
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        if let signInViewController = sender as? SignInViewController {
+            navigationController?.navigationBarHidden = false
+        }
+    }
+    
+    // MARK: - private methods
+    
+    
+    // MARK: - Actions
+    
+    @IBAction func decryptAction(sender: UIButton) {
+    }
+    
+    @IBAction func rememberButtonAction(sender: UIButton) {
+    }
+    
+}
