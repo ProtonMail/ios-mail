3a3d16b1bec6c32288064488dbb40d43b9d7ae30
Eric Chamberlain <eric.chamberlain@arctouch.com>
2015-02-15 13:25:48 -0700

    PRO-42 - Add SwiftPGP to project PRO-43 - Add SwiftPGP unit tests
    Added String extension for OpenPGP encryption and decryption.
    
    Added unit tests for OpenPGP methods.

--- ProtonMail/bin/include/OpenPGP/OpenPGP.h
+++ ProtonMail/bin/include/OpenPGP/OpenPGP.h
@@ -8,28 +8,21 @@
 
 #import <Foundation/Foundation.h>
 
-
-
 @interface OpenPGP : NSObject
 
-//issues:
-// The NSError doesn't work on swift
-//
-
-
-// self keys setup up for login user
+/// self keys setup up for login user
 - (BOOL)SetupKeys:(NSString *)priv_key pubKey:(NSString *)pub_key pass:(NSString *)passphrase error:(NSError **)err;
 
-//Encrypt for user self
+/// Encrypt for user self
 - (NSString *)encrypt_message:(NSString *)unencrypt_message error:(NSError **)err;
 
-//Encrypt message use public key(other people's key)
+/// Encrypt message use public key(other people's key)
 - (NSString *)encrypt_message:(NSString *)unencrypt_message pub_key:(NSString *)pub_key error:(NSError**) err;
 
-//Decrypt message user Private key
+/// Decrypt message user Private key
 - (NSString *)decrypt_message:(NSString*)encrypted_message error:(NSError **)err;
 
-// print debug logs
+/// print debug logs
 - (void)EnableDebug:(BOOL)isDebug;
 
 @end
