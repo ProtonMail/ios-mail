5ef8dd4fa488668936d560283c28dce035b65aae
zhj4478 <zhj4478@gmail.com>
2015-07-20 12:39:45 -0700

    MOBA-373 #comment new message can't send

--- ProtonMail/ProtonMail/MessageAPI.swift
+++ ProtonMail/ProtonMail/MessageAPI.swift
@@ -67,14 +67,11 @@ public class MessageDraftRequest<T: ApiResponse>  : ApiRequest<T> {
         messsageDict["ToList"]                  = message.recipientList.parseJson()
         messsageDict["CCList"]                  = message.ccList.parseJson()
         messsageDict["BCCList"]                 = message.bccList.parseJson()
-        
         var out : [String : AnyObject] = ["Message" : messsageDict]
-        
-        if message.orginalMessageID != nil {
+        if !(message.orginalMessageID?.isEmpty != nil) {
             out["ParentID"] = message.orginalMessageID
             out["Action"] = message.action ?? "0"   //{0|1|2} // Optional, reply = 0, reply all = 1, forward = 2
         }
-        
         
         PMLog.D(self.JSONStringify(out, prettyPrinted: true))
         return out
