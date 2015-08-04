740c4b64663ef7275da285599f858848032e72a4
zhj4478 <zhj4478@gmail.com>
2015-08-04 11:50:03 -0700

    update compose view

--- ProtonMail/ProtonMail/HtmlEditorViewController.swift
+++ ProtonMail/ProtonMail/HtmlEditorViewController.swift
@@ -14,10 +14,10 @@ protocol HtmlEditorViewControllerDelegate {
 }
 
 class HtmlEditorViewController: ZSSRichTextEditor {
-
     var delegate: HtmlEditorViewControllerDelegate?
     var emailHeader : UIView!
     var webView : UIWebView!
+    private var composeView : ComposeViewN!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -31,20 +31,48 @@ class HtmlEditorViewController: ZSSRichTextEditor {
         let w = UIScreen.mainScreen().applicationFrame.width;
         self.emailHeader.frame = CGRect(x: 0, y: 0, width: w, height: 100)
         updateContentLayout(false)
+        
+        webView.scrollView.scrollsToTop = true;
 
+        configureNavigationBar()
+        setNeedsStatusBarAppearanceUpdate()
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "statusBarHit:", name: "touchStatusBarClick", object:nil)
+    }
+    
+    override func viewWillDisappear(animated: Bool) {
+        NSNotificationCenter.defaultCenter().removeObserver(self, name: "touchStatusBarClick", object:nil)
+    }
+    
+    internal func statusBarHit (notify: NSNotification) {
+        webView.scrollView.setContentOffset(CGPoint(x: 0, y: 0), animated: true)
+    }
+    
+    override func preferredStatusBarStyle() -> UIStatusBarStyle {
+        return UIStatusBarStyle.LightContent
+    }
+    
+    func configureNavigationBar() {
+        self.navigationController?.navigationBar.barStyle = UIBarStyle.Black
+        self.navigationController?.navigationBar.barTintColor = UIColor.ProtonMail.Blue_475F77
+        self.navigationController?.navigationBar.translucent = false
+        self.navigationController?.navigationBar.tintColor = UIColor.whiteColor()
+        
+        let navigationBarTitleFont = UIFont.robotoLight(size: UIFont.Size.h2)
+        self.navigationController?.navigationBar.titleTextAttributes = [
+            NSForegroundColorAttributeName: UIColor.whiteColor(),
+            NSFontAttributeName: navigationBarTitleFont
+        ]
     }
     
     func setBody (body : String ) {
         self.setHTML(body)
     }
     
-    override func viewWillAppear(animated: Bool) {
-       // updateContentLayout(false)
-    }
-
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
     override func editorDidScrollWithPosition(position: Int) {
@@ -56,17 +84,14 @@ class HtmlEditorViewController: ZSSRichTextEditor {
         
         //self.delegate?.editorCaretPosition(new_position)
     }
-
     
     private func updateContentLayout(animation: Bool) {
-      
         UIView.animateWithDuration(animation ? 0.3 : 0, animations: { () -> Void in
             for subview in self.webView.scrollView.subviews {
                 let sub = subview as! UIView
                 if sub == self.emailHeader {
                     continue
                 } else if subview is UIImageView {
-                    //sub.hidden = true
                     continue
                 } else {
 
@@ -75,5 +100,42 @@ class HtmlEditorViewController: ZSSRichTextEditor {
                 }
             }
         })
+    }
+
+    
+    @IBAction func send_clicked(sender: AnyObject) {
+        
+    }
+    
+    @IBAction func cancel_clicked(sender: AnyObject) {
+        
+        let dismiss: (() -> Void) = {
+           // if self.viewModel.messageAction == ComposeMessageAction.OpenDraft {
+                self.navigationController?.popViewControllerAnimated(true)
+           // } else {
+                self.dismissViewControllerAnimated(true, completion: nil)
+           // }
+        }
+        
+//        if self.viewModel.hasDraft || composeView.hasContent || ((attachments?.count ?? 0) > 0) {
+//            let alertController = UIAlertController(title: NSLocalizedString("Confirmation"), message: nil, preferredStyle: .ActionSheet)
+//            alertController.addAction(UIAlertAction(title: NSLocalizedString("Save draft"), style: .Default, handler: { (action) -> Void in
+//                self.stopAutoSave()
+//                self.collectDraft()
+//                self.viewModel.updateDraft()
+//                dismiss()
+//            }))
+//            alertController.addAction(UIAlertAction(title: NSLocalizedString("Cancel"), style: .Cancel, handler: nil))
+//            alertController.addAction(UIAlertAction(title: NSLocalizedString("Discard draft"), style: .Destructive, handler: { (action) -> Void in
+//                self.stopAutoSave()
+//                self.viewModel.deleteDraft()
+//                dismiss()
+//            }))
+//            
+//            presentViewController(alertController, animated: true, completion: nil)
+//        } else {
+            dismiss()
+//        }
+
     }
 }
