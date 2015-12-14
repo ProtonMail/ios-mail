a8a596a137abe6273937666d6f18aa53d28f3fc6
zhj4478 <zhj4478@gmail.com>
2015-12-14 15:32:33 -0800

    add welcome view

---
+++ ProtonMail/ProtonMail/WelcomeViewController.swift
@@ -0,0 +1,27 @@
+//
+//  WelcomeViewController.swift
+//  
+//
+//  Created by Yanfeng Zhang on 12/14/15.
+//
+//
+
+
+import UIKit
+
+class WelcomeViewController: UIViewController {
+    
+    
+    @IBOutlet weak var createAccountButton: UIButton!
+    
+    @IBOutlet weak var signInButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+        
+    
+    }
+
+
+}
