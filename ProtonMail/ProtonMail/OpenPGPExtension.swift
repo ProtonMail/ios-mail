8b44ab2505f19ea8d9f0ed346e69c76215ef8e36
zhj4478 <zhj4478@gmail.com>
2016-01-15 16:56:56 -0800

    update app to handle more errors

--- ProtonMail/ProtonMail/OpenPGPExtension.swift
+++ ProtonMail/ProtonMail/OpenPGPExtension.swift
@@ -33,7 +33,7 @@ extension PMNOpenPgp {
         }
         return true
     }
-
+    
     func generateKey(passphrase: String, userName: String, error: NSErrorPointer?) -> PMNOpenPgpKey? {
         let key = generateKey(userName, domain: "protonmail.com", passphrase: passphrase)
         if key.privateKey.isEmpty || key.publicKey.isEmpty {
@@ -44,19 +44,19 @@ extension PMNOpenPgp {
     
     func updatePassphrase(privateKey: String, publicKey: String, old_pass: String, new_pass: String, error: NSErrorPointer?) -> String? {
         //TODO:: need do migrate
-//        var anError: NSError?
-//        if !SetupKeys(privateKey, pubKey: publicKey, pass: old_pass, error: &anError) {
-//            if let error = error {
-//                error.memory = anError
-//            }
-//            return nil
-//        }
-//        if let new_privkey = update_key_password(old_pass, new_pwd: new_pass, error: &anError) {
-//            return new_privkey
-//        }
-//        if let error = error {
-//            error.memory = anError
-//        }
+        //        var anError: NSError?
+        //        if !SetupKeys(privateKey, pubKey: publicKey, pass: old_pass, error: &anError) {
+        //            if let error = error {
+        //                error.memory = anError
+        //            }
+        //            return nil
+        //        }
+        //        if let new_privkey = update_key_password(old_pass, new_pwd: new_pass, error: &anError) {
+        //            return new_privkey
+        //        }
+        //        if let error = error {
+        //            error.memory = anError
+        //        }
         return nil
     }
 }
@@ -65,8 +65,17 @@ extension PMNOpenPgp {
 
 extension String {
     func decryptWithPrivateKey(privateKey: String, passphrase: String, error: NSErrorPointer?) -> String? {
-        let decrypt = sharedOpenPGP.decryptMessageSingleKey(self, privateKey: privateKey, passphras: passphrase)
-        return decrypt
+        
+        var decrypted : String?;
+        SwiftTryCatch.tryBlock({ () -> Void in
+            decrypted = sharedOpenPGP.decryptMessageSingleKey(self, privateKey: privateKey, passphras: passphrase)
+            }, catchBlock: { (exc) -> Void in
+                if let error = error {
+                    error.memory = exc.toError()
+                }
+            }) { () -> Void in
+        }
+        return decrypted;
     }
     
     func encryptWithPublicKey(publicKey: String, error: NSErrorPointer?) -> String? {
@@ -92,45 +101,45 @@ extension NSData {
         return out_enc_out
     }
     
-//    func encryptWithPublicKeys(publicKeys: NSMutableDictionary, fileName:String, error: NSErrorPointer?) -> NSMutableDictionary? {
-//        let publicKey = publicKeys["self"] as! String;
-//
-//        sharedOpenPGP.encryptAttachmentSingleKey(publicKey, unencryptData: self, fileName: fileName)
-//        
-////        var out_packet [String, PMNEncryptPackage] =
-////        for (key, value) in publicKeys {
-////            
-////            
-////            sharedOpenPGP.encryptAttachmentSingleKey(<#publicKey: String#>, unencryptData: <#NSData#>, fileName: <#String#>)
-////        }
-////        
-//        
-////        - (NSMutableDictionary*) encrypt_attachments:(NSData *)unencrypt_att fileNam:(NSString*)name pub_keys:(NSMutableDictionary*)pub_keys error:(NSError**) err
-////        {
-////            NSMutableDictionary *dictX = [[NSMutableDictionary alloc] init];
-////            
-////            std::string unencrypt_attachment = std::string((char* )[unencrypt_att bytes], [unencrypt_att length]);
-////            std::string session_key = generat_session_key();
-////            std::string fileName = [name UTF8String];
-////            
-////            for(id key in pub_keys)
-////            {
-////                std::string user_pub_key = [pub_keys[key] UTF8String];
-////                PGPPublicKey pub(user_pub_key);
-////                PGPMessage enrypted_session_key = encrypt_pka_only_session(pub, session_key);
-////                std::string enrypted_session_key_data = enrypted_session_key.write(1);
-////                [dictX setObject:[NSData dataWithBytes: enrypted_session_key_data.c_str() length:enrypted_session_key_data.length()] forKey:key];
-////            }
-////            
-////            PGPMessage encrypted_att = encrypt_pka_only_data(session_key, unencrypt_attachment, fileName, 9, 0);
-////            std::string endryp_dat = encrypted_att.write(1);
-////            [dictX setObject:[NSData dataWithBytes: endryp_dat.c_str() length:endryp_dat.length()] forKey:@"DataPacket"];
-////            
-////            return dictX;
-////        }
-//        return nil
-//    }
-
+    //    func encryptWithPublicKeys(publicKeys: NSMutableDictionary, fileName:String, error: NSErrorPointer?) -> NSMutableDictionary? {
+    //        let publicKey = publicKeys["self"] as! String;
+    //
+    //        sharedOpenPGP.encryptAttachmentSingleKey(publicKey, unencryptData: self, fileName: fileName)
+    //
+    ////        var out_packet [String, PMNEncryptPackage] =
+    ////        for (key, value) in publicKeys {
+    ////
+    ////
+    ////            sharedOpenPGP.encryptAttachmentSingleKey(<#publicKey: String#>, unencryptData: <#NSData#>, fileName: <#String#>)
+    ////        }
+    ////
+    //
+    ////        - (NSMutableDictionary*) encrypt_attachments:(NSData *)unencrypt_att fileNam:(NSString*)name pub_keys:(NSMutableDictionary*)pub_keys error:(NSError**) err
+    ////        {
+    ////            NSMutableDictionary *dictX = [[NSMutableDictionary alloc] init];
+    ////
+    ////            std::string unencrypt_attachment = std::string((char* )[unencrypt_att bytes], [unencrypt_att length]);
+    ////            std::string session_key = generat_session_key();
+    ////            std::string fileName = [name UTF8String];
+    ////
+    ////            for(id key in pub_keys)
+    ////            {
+    ////                std::string user_pub_key = [pub_keys[key] UTF8String];
+    ////                PGPPublicKey pub(user_pub_key);
+    ////                PGPMessage enrypted_session_key = encrypt_pka_only_session(pub, session_key);
+    ////                std::string enrypted_session_key_data = enrypted_session_key.write(1);
+    ////                [dictX setObject:[NSData dataWithBytes: enrypted_session_key_data.c_str() length:enrypted_session_key_data.length()] forKey:key];
+    ////            }
+    ////
+    ////            PGPMessage encrypted_att = encrypt_pka_only_data(session_key, unencrypt_attachment, fileName, 9, 0);
+    ////            std::string endryp_dat = encrypted_att.write(1);
+    ////            [dictX setObject:[NSData dataWithBytes: endryp_dat.c_str() length:endryp_dat.length()] forKey:@"DataPacket"];
+    ////
+    ////            return dictX;
+    ////        }
+    //        return nil
+    //    }
+    
     func getSessionKeyFromPubKeyPackage(privateKey: String, passphrase: String, publicKey: String, error: NSErrorPointer?) -> NSData? {
         let key_session_out = sharedOpenPGP.getPublicKeySessionKey(self, privateKey: privateKey, passphrase: passphrase)
         return key_session_out
