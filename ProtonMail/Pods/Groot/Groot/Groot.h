4b2c8b532fc9130de4a73d840b2cef6a7810cae8
zhj4478 <zhj4478@gmail.com>
2016-11-30 11:44:25 -0800

    update pod Groot to 2.0

--- ProtonMail/Pods/Groot/Groot/Groot.h
+++ ProtonMail/Pods/Groot/Groot/Groot.h
@@ -1,6 +1,6 @@
 // Groot.h
 //
-// Copyright (c) 2014 Guillermo Gonzalez
+// Copyright (c) 2014-2016 Guillermo Gonzalez
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
--- ProtonMail/Pods/Groot/Groot/Private/GRTUniquingSerializationStrategy.h
+++ ProtonMail/Pods/Groot/Groot/Private/GRTUniquingSerializationStrategy.h
@@ -1,6 +1,6 @@
 // GRTUniquingSerializationStrategy.h
 //
-// Copyright (c) 2014-2015 Guillermo Gonzalez
+// Copyright (c) 2014-2016 Guillermo Gonzalez
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
