979cfb9c2c792303dd6c768e83df3da5aa597bed
zhj4478 <zhj4478@gmail.com>
2017-10-18 12:11:03 -0700

    update overlap issue

--- ProtonMail/ProtonMail/MenuViewController.swift
+++ ProtonMail/ProtonMail/MenuViewController.swift
@@ -82,8 +82,7 @@ class MenuViewController: UIViewController {
         sharedLabelsDataService.fetchLabels();
     }
     
-    @objc func performLastSegue(_ notification: Notification)
-    {
+    @objc func performLastSegue(_ notification: Notification) {
         self.performSegue(withIdentifier: lastSegue, sender: IndexPath(row: 0, section: 0))
     }
     
@@ -104,7 +103,6 @@ class MenuViewController: UIViewController {
         } else {
             otherItems = [MenuItem.contacts, MenuItem.settings, MenuItem.bugs, /*MenuItem.feedback,*/ MenuItem.signout]
         }
-        
         
         updateEmailLabel()
         updateDisplayNameLabel()
