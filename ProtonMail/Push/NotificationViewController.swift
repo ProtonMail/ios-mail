7eafd2c79ee854d6c98676679050d0dd191e522a
zhj4478 <zhj4478@gmail.com>
2017-11-13 17:26:55 -0800

    init push targets for live and beta

---
+++ ProtonMail/Push/NotificationViewController.swift
@@ -0,0 +1,26 @@
+//
+//  NotificationViewController.swift
+//  Push
+//
+//  Created by Yanfeng Zhang on 11/13/17.
+//  Copyright © 2017 ProtonMail. All rights reserved.
+//
+
+import UIKit
+import UserNotifications
+import UserNotificationsUI
+
+class NotificationViewController: UIViewController, UNNotificationContentExtension {
+
+    @IBOutlet var label: UILabel?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any required interface initialization here.
+    }
+    
+    func didReceive(_ notification: UNNotification) {
+        self.label?.text = notification.request.content.body
+    }
+
+}
