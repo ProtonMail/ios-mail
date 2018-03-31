dd61a58524780febe21da758d9c9bd46a10e88a5
zhj4478 <zhj4478@gmail.com>
2018-03-30 18:49:13 -0700

    fix build when update to 11.3

--- ProtonMail/ProtonMail/ComposeViewModelImpl.swift
+++ ProtonMail/ProtonMail/ComposeViewModelImpl.swift
@@ -423,7 +423,10 @@ final class ComposeViewModelImpl : ComposeViewModel {
                 let sn : String! = (message?.managedObjectContext != nil) ? message!.senderContactVO.name : "unknow"
                 let se : String! = message?.managedObjectContext != nil ? message!.senderContactVO.email : "unknow"
                 
-                var replyHeader = time + ", " + sn! + " &lt;<a href=\"mailto:" + se + "\" class=\"\">" + se + "</a>&gt;"
+                var replyHeader = time + ", " + sn!
+                replyHeader = replyHeader + " &lt;<a href=\"mailto:"
+                replyHeader = replyHeader + se + "\" class=\"\">" + se + "</a>&gt;"
+                
                 replyHeader = replyHeader.stringByStrippingStyleHTML()
                 replyHeader = replyHeader.stringByStrippingBodyStyle()
                 replyHeader = replyHeader.stringByPurifyHTML()
