89fb05796b2c0cf025826173011f2049dc28a0d7
zhj4478 <zhj4478@gmail.com>
2018-06-28 19:07:26 -0700

    test push

---
+++ ProtonMail/ProtonMail/OpenPGP+String+Extension.swift
@@ -0,0 +1,81 @@
+
+//
+//  OpenPGPExtension.swift
+//  ProtonMail
+//
+//
+// Copyright 2015 ArcTouch, Inc.
+// All rights reserved.
+//
+// This file, its contents, concepts, methods, behavior, and operation
+// (collectively the "Software") are protected by trade secret, patent,
+// and copyright laws. The use of the Software is governed by a license
+// agreement. Disclosure of the Software to third parties, in any form,
+// in whole or in part, is expressly prohibited except as authorized by
+// the license agreement.
+//
+
+import Foundation
+import Pm
+
+let sharedOpenPGP = PmOpenPGP()!
+
+
+// MARK: - OpenPGP String extension
+
+extension String {
+    
+    func getSignature() throws -> String? {
+        var error : NSError?
+        let dec_out_att : String = PmReadClearSignedMessage(self, &error)
+        if let err = error {
+            throw err
+        }
+        return dec_out_att
+    }
+    
+    func decryptMessage(binKeys: Data, passphrase: String) throws -> String? {
+        return try sharedOpenPGP.decryptMessageBinKey(self, privateKey: binKeys, passphrase: passphrase)
+    }
+    
+    func verifyMessage(verifier: Data, binKeys: Data, passphrase: String) throws -> PmDecryptSignedVerify? {
+        return try sharedOpenPGP.decryptMessageVerifyBinKeyPrivbinkeys(self, veriferKey: verifier, privateKeys: binKeys, passphrase: passphrase, verifyTime: 0)
+    }
+    
+    func decryptMessageWithSinglKey(_ privateKey: String, passphrase: String) throws -> String? {
+        return try sharedOpenPGP.decryptMessage(self, privateKey: privateKey, passphrase: passphrase)
+    }
+    
+    func split() throws -> PmEncryptedSplit? {
+        var error : NSError?
+        let out = PmSeparateKeyAndData(self, &error)
+        if let err = error {
+            throw err
+        }
+        return out
+    }
+    
+    func encrypt(withAddr address_id: String, mailbox_pwd: String) throws -> String? {
+        let privateKey = sharedUserDataService.getAddressPrivKey(address_id: address_id)
+        return try sharedOpenPGP.encryptMessage(self, publicKey: privateKey, privateKey: privateKey, passphrase: mailbox_pwd, trim: true)
+    }
+    
+    func encrypt(withPubKey publicKey: String, privateKey: String, mailbox_pwd: String) throws -> String? {
+        return try sharedOpenPGP.encryptMessage(self, publicKey: publicKey, privateKey: privateKey, passphrase: mailbox_pwd, trim: true)
+    }
+    
+    func encrypt(withPwd passphrase: String) throws -> String? {
+        return try sharedOpenPGP.encryptMessage(withPassword: self, password: passphrase)
+    }
+    
+    func decrypt(withPwd passphrase: String) throws -> String? {
+        return try sharedOpenPGP.decryptMessage(withPassword: self, password: passphrase)
+    }
+    
+    //self is private key
+    func check(passphrase: String) -> Bool {
+        return PmCheckPassphrase(self, passphrase)
+    }
+}
+
+
---
+++ ProtonMail/ProtonMailCommon/UpdateTime.swift
@@ -0,0 +1,254 @@
+//
+//  LastUpdatedStore.swift
+//  ProtonMail
+//
+//
+// Copyright 2015 ArcTouch, Inc.
+// All rights reserved.
+//
+// This file, its contents, concepts, methods, behavior, and operation
+// (collectively the "Software") are protected by trade secret, patent,
+// and copyright laws. The use of the Software is governed by a license
+// agreement. Disclosure of the Software to third parties, in any form,
+// in whole or in part, is expressly prohibited except as authorized by
+// the license agreement.
+
+
+
+import Foundation
+
+let lastUpdatedStore = LastUpdatedStore()
+final class UpdateTime : NSObject {
+    var start : Date
+    var end : Date
+    var update : Date
+    var total : Int32
+    var unread : Int32
+
+    required init (start: Date!, end : Date, update : Date, total : Int32, unread: Int32){
+        self.start = start
+        self.end = end
+        self.update = update
+        self.unread = unread
+        self.total = total
+    }
+    
+    var isNew : Bool {
+        get{
+            return  self.start == self.end && self.start == self.update
+        }
+    }
+    
+    static func distantPast() -> UpdateTime {
+        return UpdateTime(start: Date.distantPast , end: Date.distantPast , update: Date.distantPast , total: 0, unread: 0)
+    }
+}
+
+extension UpdateTime : NSCoding {
+    
+    fileprivate struct CoderKey {
+        static let startCode = "start"
+        static let endCode = "end"
+        static let updateCode = "update"
+        static let unread = "unread"
+        static let total = "total"
+    }
+    
+    convenience init(coder aDecoder: NSCoder) {
+        self.init(
+            start: aDecoder.decodeObject(forKey: CoderKey.startCode) as! Date,
+            end: aDecoder.decodeObject(forKey: CoderKey.endCode) as! Date,
+            update: aDecoder.decodeObject(forKey: CoderKey.updateCode) as! Date,
+            total: aDecoder.decodeInt32(forKey: CoderKey.total),
+            unread: aDecoder.decodeInt32(forKey: CoderKey.unread))
+    }
+    
+    func encode(with aCoder: NSCoder) {
+        aCoder.encode(self.start, forKey: CoderKey.startCode)
+        aCoder.encode(self.end, forKey: CoderKey.endCode)
+        aCoder.encode(self.update, forKey: CoderKey.updateCode)
+        aCoder.encode(self.total, forKey: CoderKey.total)
+        aCoder.encode(self.unread, forKey: CoderKey.unread)
+    }
+}
+
+final class LastUpdatedStore : SharedCacheBase {
+    
+    fileprivate struct Key {
+        static let labelsUnreadCount   = "LabelsUnreadCount"  //for inbox & labels
+        static let lastLabelsUpdated   = "LastLabelsUpdated" //for inbox & labels
+        
+        static let unreadMessageCount  = "unreadMessageCount"  //total unread
+        
+        static let lastEventID         = "lastEventID"  //
+        static let lastCantactsUpdated = "LastCantactsUpdated" //
+        
+        //added 1.8.0 new contacts
+        static let isContactsCached    = "isContactsCached"  //
+        
+        //Removed at 1.5.5 still need for cleanup
+        static let mailboxUnreadCount  = "MailboxUnreadCount"
+        static let lastInboxesUpdated  = "LastInboxesUpdated"
+    }
+    
+    var lastLabelsUpdateds: [String : UpdateTime] {
+        get {
+            return (getShared().customObjectForKey(Key.lastLabelsUpdated) as? [String : UpdateTime]) ?? [:]
+        }
+        set {
+            getShared().setCustomValue(newValue as NSCoding?, forKey: Key.lastLabelsUpdated)
+            getShared().synchronize()
+        }
+    }
+    
+    var labelsUnreadCounts: [String : Int] {
+        get {
+            return (getShared().customObjectForKey(Key.labelsUnreadCount) as? [String : Int]) ?? [:]
+        }
+        set {
+            getShared().setCustomValue(newValue as NSCoding?, forKey: Key.labelsUnreadCount)
+            getShared().synchronize()
+        }
+    }
+    
+    var lastEventID: String! {
+        get {
+            let eid =  getShared().string(forKey: Key.lastEventID) ?? "0"
+            return eid
+        }
+        set {
+            getShared().setValue(newValue, forKey: Key.lastEventID)
+            getShared().synchronize()
+        }
+    }
+    
+    var contactsCached: Int {
+        get {
+            return getShared().integer(forKey: Key.isContactsCached)
+        }
+        set {
+            getShared().setValue(newValue, forKey: Key.isContactsCached)
+            getShared().synchronize()
+        }
+    }
+    
+    var totalUnread: Int! {
+        get {
+            return getShared().integer(forKey: Key.unreadMessageCount) 
+        }
+        set {
+            getShared().setValue(newValue, forKey: Key.unreadMessageCount)
+            getShared().synchronize()
+        }
+    }
+
+    
+    /**
+    clear the last update time cache
+    */
+    func clear() {
+        //in use
+        getShared().removeObject(forKey: Key.lastCantactsUpdated)
+        getShared().removeObject(forKey: Key.lastLabelsUpdated)
+        getShared().removeObject(forKey: Key.lastEventID)
+        getShared().removeObject(forKey: Key.unreadMessageCount)
+        getShared().removeObject(forKey: Key.labelsUnreadCount)
+        getShared().removeObject(forKey: Key.isContactsCached)
+        
+        //removed
+        getShared().removeObject(forKey: Key.mailboxUnreadCount)
+        getShared().removeObject(forKey: Key.lastInboxesUpdated)
+        
+        //sync
+        getShared().synchronize()
+    }
+    
+    
+    /**
+    get the inbox last update time by location
+    
+    :param: location MessageLocation
+    
+    :returns: the Update Time
+    */
+    func inboxLastForKey(_ location : MessageLocation) -> UpdateTime {
+        let str_location = String(location.rawValue)
+        return lastLabelsUpdateds[str_location] ?? UpdateTime.distantPast()
+    }
+    
+    /**
+    update the exsit inbox last update time by location
+    
+    :param: location   message location
+    :param: updateTime the new update time
+    */
+    func updateInboxForKey(_ location : MessageLocation, updateTime: UpdateTime) -> Void {
+        let str_location = String(location.rawValue)
+        return lastLabelsUpdateds[str_location] = updateTime
+    }
+    
+    func labelsLastForKey(_ labelID : String) -> UpdateTime {
+        return lastLabelsUpdateds[labelID] ?? UpdateTime.distantPast()
+    }
+    
+    func updateLabelsForKey(_ labelID : String, updateTime: UpdateTime) -> Void {
+        return lastLabelsUpdateds[labelID] = updateTime
+    }
+    
+    
+    // location & label: message unread count
+    func UnreadCountForKey(_ labelID : String) -> Int {
+        return labelsUnreadCounts[labelID] ?? 0
+    }
+    
+    func UnreadCountForKey(_ location : MessageLocation) -> Int {
+        let str_location = String(location.rawValue)
+        return labelsUnreadCounts[str_location] ?? 0
+    }
+    
+    func updateLabelsUnreadCountForKey(_ labelID : String, count: Int) -> Void {
+        return labelsUnreadCounts[labelID] = count
+    }
+    
+    func updateUnreadCountForKey(_ location : MessageLocation, count: Int) -> Void {
+        let str_location = String(location.rawValue)
+        return labelsUnreadCounts[str_location] = count
+    }
+    
+    // Mailbox unread count change
+    func UnreadMailboxMessage(_ location : MessageLocation) {
+        let str_location = String(location.rawValue)
+        var currentCount = labelsUnreadCounts[str_location] ?? 0
+        currentCount += 1;
+        labelsUnreadCounts[str_location] = currentCount
+    }
+    
+    func ReadMailboxMessage(_ location : MessageLocation) {
+        let str_location = String(location.rawValue)
+        var currentCount = labelsUnreadCounts[str_location] ?? 0
+        currentCount -= 1;
+        if currentCount < 0 {
+            currentCount = 0
+        }
+        labelsUnreadCounts[str_location] = currentCount
+    }
+    
+    func MoveUnReadMailboxMessage(_ from : MessageLocation, to : MessageLocation) {
+        UnreadMailboxMessage(from);
+        ReadMailboxMessage(to)
+    }
+    
+    
+    // reset functions    
+    func resetUnreadCounts() {
+        getShared().removeObject(forKey: Key.mailboxUnreadCount)
+        getShared().removeObject(forKey: Key.labelsUnreadCount)
+        getShared().removeObject(forKey: Key.lastCantactsUpdated)
+        getShared().removeObject(forKey: Key.unreadMessageCount)
+        
+        getShared().synchronize()
+    }
+}
+
+
+
