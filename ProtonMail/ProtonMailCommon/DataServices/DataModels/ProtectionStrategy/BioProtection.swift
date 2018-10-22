13afb0c51b57d58fbd332bff8934359acdd4a1ec
Anatoly Rosencrantz <rosencrantz@protonmail.com>
2018-10-22 16:31:02 +0300

    legacy BioProtection for devices uncapable of SE key storing (before iOS 10.3)

--- ProtonMail/ProtonMailCommon/DataServices/DataModels/ProtectionStrategy/BioProtection.swift
+++ ProtonMail/ProtonMailCommon/DataServices/DataModels/ProtectionStrategy/BioProtection.swift
@@ -13,11 +13,15 @@ import EllipticCurveKeyPair
 import UICKeyChainStore
 
 struct BioProtection: ProtectionStrategy {
+    private static var privateLabelKey = String(describing: BioProtection.self) + ".private"
+    private static var publicLabelKey = String(describing: BioProtection.self) + ".public"
+    private static var legacyLabelKey = String(describing: BioProtection.self) + ".legacy"
+    
     private static func makeAsymmetricEncryptor() -> EllipticCurveKeyPair.Manager {
         let publicAccessControl = EllipticCurveKeyPair.AccessControl(protection: kSecAttrAccessibleAlwaysThisDeviceOnly, flags: [.userPresence, .privateKeyUsage])
         let privateAccessControl = EllipticCurveKeyPair.AccessControl(protection: kSecAttrAccessibleAfterFirstUnlockThisDeviceOnly, flags: [.userPresence, .privateKeyUsage])
-        let config = EllipticCurveKeyPair.Config(publicLabel: String(describing: BioProtection.self) + ".public",
-                                                  privateLabel: String(describing: BioProtection.self) + ".private",
+        let config = EllipticCurveKeyPair.Config(publicLabel: self.publicLabelKey,
+                                                  privateLabel: self.privateLabelKey,
                                                   operationPrompt: "MUCH IMPORTANT SO NEED",
                                                   publicKeyAccessControl: publicAccessControl,
                                                   privateKeyAccessControl: privateAccessControl,
@@ -27,13 +31,33 @@ struct BioProtection: ProtectionStrategy {
         return EllipticCurveKeyPair.Manager(config: config)
     }
     
+    
+    // for iOS older than 10.3 - not capable of elliptic curve encryption
+    private static func makeSymmetricEncryptor() -> Keymaker.Key {
+        guard let key = self.keychain.data(forKey: self.legacyLabelKey) else {
+            let oldAccessibility = self.keychain.accessibility
+            let oldAuthPolicy = self.keychain.authenticationPolicy
+            
+            self.keychain.setAccessibility(.afterFirstUnlockThisDeviceOnly, authenticationPolicy: .userPresence)
+            
+            let ethemeralKey = self.generateRandomValue(length: 32)
+            self.keychain.setData(Data(bytes: ethemeralKey), forKey: self.legacyLabelKey)
+
+            self.keychain.setAccessibility(oldAccessibility, authenticationPolicy: oldAuthPolicy)
+            return ethemeralKey
+        }
+        return key.bytes
+    }
+    
     func lock(value: Keymaker.Key) throws {
         let locked = try Locked<Keymaker.Key>(clearValue: value) { cleartext -> Data in
             if #available(iOS 10.3, *) {
                 let encryptor = BioProtection.makeAsymmetricEncryptor()
                 return try encryptor.encrypt(Data(bytes: cleartext))
             } else {
-                fatalError("pre ios10.3")
+                let ethemeral = BioProtection.makeSymmetricEncryptor()
+                let locked = try Locked(clearValue: cleartext, with: ethemeral)
+                return locked.encryptedValue
             }
         }
         
@@ -47,7 +71,8 @@ struct BioProtection: ProtectionStrategy {
                 let encryptor = BioProtection.makeAsymmetricEncryptor()
                 return try encryptor.decrypt(cyphertext).bytes
             } else {
-                fatalError("pre ios10.3")
+                let ethemeral = BioProtection.makeSymmetricEncryptor()
+                return try locked.unlock(with: ethemeral)
             }
         }
         
@@ -57,6 +82,7 @@ struct BioProtection: ProtectionStrategy {
     static func removeCyphertextFromKeychain() {
         (self as ProtectionStrategy.Type).removeCyphertextFromKeychain()
         try? BioProtection.makeAsymmetricEncryptor().deleteKeyPair()
+        self.keychain.removeItem(forKey: self.legacyLabelKey)
     }
 }
 
