97857cc031b423a0f4a6b2d41b47392879494dbc
Anatoly Rosencrantz <rosencrantz@protonmail.com>
2018-11-21 18:21:15 +0200

    fix error message presentation

--- ProtonMail/ProtonMail/Controller/ServiceLevel/ServiceLevelViewController/ServiceLevelViewController.swift
+++ ProtonMail/ProtonMail/Controller/ServiceLevel/ServiceLevelViewController/ServiceLevelViewController.swift
@@ -110,16 +110,14 @@ class ServiceLevelViewControllerBase: UICollectionViewController {
 extension ServiceLevelViewControllerBase: ServiceLevelDataSourceDelegate {
     func purchaseProduct(id: String) {
         let successCompletion: ()->Void = {
-            
             {
                 self.navigationController?.popViewController(animated: true)
             } ~> .main
-            
         }
         let errorCompletion: (Error)->Void = { error in
             let alert = UIAlertController(title: LocalString._error_occured, message: error.localizedDescription, preferredStyle: .alert)
             alert.addAction(.init(title: LocalString._general_ok_action, style: .cancel, handler: nil))
-            self.present(alert, animated: true, completion: nil)
+            UIApplication.shared.keyWindow?.rootViewController?.present(alert, animated: true, completion: nil)
         }
         let deferredCompletion: ()->Void = {
             
