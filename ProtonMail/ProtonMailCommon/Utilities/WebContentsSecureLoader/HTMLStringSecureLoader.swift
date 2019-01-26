684a6789a0fb46b486299d86bb1f876f96b15fe5
Anatoly Rosencrantz <rosencrantz@protonmail.com>
2019-01-26 10:36:36 +0200

    add spacer to message body on ios9-11

--- ProtonMail/ProtonMailCommon/Utilities/WebContentsSecureLoader/HTMLStringSecureLoader.swift
+++ ProtonMail/ProtonMailCommon/Utilities/WebContentsSecureLoader/HTMLStringSecureLoader.swift
@@ -85,11 +85,26 @@ class HTMLStringSecureLoader: NSObject, WebContentsSecureLoader, WKScriptMessage
         metaWidth.content = metaWidth.content + ", initial-scale=" + ratio + ", maximum-scale=3.0";
         };
         document.getElementsByTagName('head')[0].appendChild(metaWidth);
+        """
+        let spacer: String = { () -> String in
+            if #available(iOS 12.0, *) {
+                return ""
+            } else {
+                return """
+                var div = document.createElement('p');
+                div.style.width = '100%';
+                div.style.height = 1000/ratio + 'px';
+                div.style.display = 'block';
+                document.body.appendChild(div);
+                """
+            }
+        }()
         
+        let message = """
         window.webkit.messageHandlers.loaded.postMessage({'clearBody':document.documentElement.innerHTML});
         """
         
-        let sanitize = WKUserScript(source: sanitizeRaw, injectionTime: .atDocumentEnd, forMainFrameOnly: false)
+        let sanitize = WKUserScript(source: sanitizeRaw + spacer + message, injectionTime: .atDocumentEnd, forMainFrameOnly: false)
         config.userContentController.removeAllUserScripts()
         config.userContentController.addUserScript(WebContents.domPurifyConstructor)
         config.userContentController.addUserScript(sanitize)
