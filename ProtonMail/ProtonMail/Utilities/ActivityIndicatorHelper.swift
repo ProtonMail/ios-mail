bcbda6ccf4e09c072a1bc4fe5a32d4f7c055a67d
Anatoly Rosencrantz <rosencrantz@protonmail.com>
2019-02-26 17:06:39 +0200

    unify ActivityIndicatoHelper and MBProgressHUD

--- ProtonMail/ProtonMail/Utilities/ActivityIndicatorHelper.swift
+++ ProtonMail/ProtonMail/Utilities/ActivityIndicatorHelper.swift
@@ -25,47 +25,13 @@
 //  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 //  THE SOFTWARE.
 
+import MBProgressHUD
 
-private let ACTIVITY_INDICATOR_TAG = 99
-private let ACTIVITY_INDICATOR_SIZE: CGFloat = 75
-private let ACTIVITY_INDICATOR_FADE_ANIMATION_DURATION = 0.25
-
+@available(*, deprecated, message: "It used to be a standalone object, now use MBProgressHUD please")
 class ActivityIndicatorHelper {
     
-    class func showActivityIndicator(at view: UIView, style: UIActivityIndicatorView.Style) {
-        view.isUserInteractionEnabled = false
-        let block = { () -> Void in
-            var activityIndicator: UIActivityIndicatorView?
-            for subview in view.subviews {
-                if (subview.tag == ACTIVITY_INDICATOR_TAG) {
-                    activityIndicator = subview as? UIActivityIndicatorView
-                }
-            }
-            
-            if (activityIndicator == nil) {
-                activityIndicator = ActivityIndicatorHelper.setupActivityIndicator(by: style)
-                view.addSubview(activityIndicator!)
-                activityIndicator!.mas_makeConstraints { (make) -> Void in
-                    make?.center.equalTo()(view)
-                    make?.width.equalTo()(ACTIVITY_INDICATOR_SIZE)
-                    make?.height.equalTo()(ACTIVITY_INDICATOR_SIZE)
-                }
-            }
-            
-            view.isUserInteractionEnabled = false
-            activityIndicator!.startAnimating()
-            UIView.animate(withDuration: ACTIVITY_INDICATOR_FADE_ANIMATION_DURATION, animations: { () -> Void in
-                activityIndicator!.alpha = 0.8
-            })
-        }
-        
-        if (Thread.current == Thread.main) {
-            block()
-        } else {
-            DispatchQueue.main.async(execute: { () -> Void in
-                block()
-            })
-        }
+    fileprivate class func showActivityIndicator(at view: UIView, style: UIActivityIndicatorView.Style) {
+        MBProgressHUD.showAdded(to: view, animated: true)
     }
     
     class func show(at view: UIView?) {
@@ -87,43 +53,6 @@ class ActivityIndicatorHelper {
     }
     
     class func hideActivityIndicator(at view: UIView) {
-        DispatchQueue.main.async(execute: { () -> Void in
-            view.isUserInteractionEnabled = true
-            
-            var activityIndicator: UIActivityIndicatorView?
-            for subview in view.subviews {
-                if (subview.tag == ACTIVITY_INDICATOR_TAG) {
-                    activityIndicator = subview as? UIActivityIndicatorView
-                }
-            }
-            
-            if (activityIndicator != nil) {
-                activityIndicator!.startAnimating()
-                UIView.animate(withDuration: ACTIVITY_INDICATOR_FADE_ANIMATION_DURATION, animations: { () -> Void in
-                    activityIndicator!.alpha = 0
-                }, completion: { (finished) -> Void in
-                    activityIndicator!.stopAnimating()
-                    view.isUserInteractionEnabled = true
-                })
-            }
-        })
-    }
-    
-    fileprivate class func setupActivityIndicator(by style: UIActivityIndicatorView.Style?) -> UIActivityIndicatorView {
-        var activityIndicator: UIActivityIndicatorView!
-        
-        if let indicatorStyle = style {
-            activityIndicator = UIActivityIndicatorView(style: indicatorStyle)
-        } else {
-            activityIndicator = UIActivityIndicatorView(style: UIActivityIndicatorView.Style.whiteLarge)
-        }
-        
-        activityIndicator.backgroundColor = UIColor.black
-        activityIndicator.alpha = 0
-        activityIndicator.layer.cornerRadius = 8
-        activityIndicator.layer.masksToBounds = true
-        activityIndicator.tag = ACTIVITY_INDICATOR_TAG
-        
-        return activityIndicator
+        MBProgressHUD.hide(for: view, animated: true)
     }
 }
