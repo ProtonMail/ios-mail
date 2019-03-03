5ceb5a35860e9b27e57179feae41416350a2ee04
Anatoly Rosencrantz <rosencrantz@protonmail.com>
2019-03-03 09:19:57 +0200

    #899 more sophisticated escaping of special characters before passing string to JS

--- ProtonMail/ProtonMail/Controller/Compose/HtmlEditor/String+Html+Ext.swift
+++ ProtonMail/ProtonMail/Controller/Compose/HtmlEditor/String+Html+Ext.swift
@@ -29,23 +29,12 @@
 import Foundation
 
 extension String {
-    /// A string with the ' characters in it escaped.
+    /// A string with the special characters in it escaped.
     /// Used when passing a string into JavaScript, so the string is not completed too soon
-    /// refer: https://github.com/cjwirth/RichEditorView
     var escaped: String {
-        let unicode = self.unicodeScalars
-        var newString = ""
-        for char in unicode {
-            if char.value == 39 || // 39 == ' in ASCII
-                char.value < 9 ||  // 9 == horizontal tab in ASCII
-                (char.value > 9 && char.value < 32) // < 32 == special characters in ASCII
-            {
-                let escaped = char.escaped(asASCII: true)
-                newString.append(escaped)
-            } else {
-                newString.append(String(char))
-            }
-        }
-        return newString
+        let mutableString = NSMutableString(string: self)
+        CFStringTransform(mutableString, nil, "Any-Hex/Java" as NSString, false)
+        
+        return mutableString as String
     }
 }
