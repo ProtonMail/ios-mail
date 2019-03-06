8552d16a41dd6157b8ff452bed836ed9feb343e2
Anatoly Rosencrantz <rosencrantz@protonmail.com>
2019-03-06 17:01:44 +0200

    MessageViewController will be UITableViewController now

---
+++ ProtonMail/ProtonMail/Controller/Message Detail/ModernMessageViewController.swift
@@ -0,0 +1,67 @@
+//
+//  ModernMessageViewController.swift
+//  ProtonMail - Created on 06/03/2019.
+//
+//
+//  The MIT License
+//
+//  Copyright (c) 2018 Proton Technologies AG
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//  THE SOFTWARE.
+
+
+import UIKit
+
+class MessageViewController: UITableViewController, ViewModelProtocol, ProtonMailViewControllerProtocol {
+    
+    // base protocols
+    override var preferredStatusBarStyle : UIStatusBarStyle {
+        return UIStatusBarStyle.lightContent
+    }
+    @IBOutlet weak var menuButton: UIBarButtonItem!
+    func configureNavigationBar() {
+        ProtonMailViewController.configureNavigationBar(self)
+    }
+    
+    // legacy
+    typealias viewModelType = MessageViewModel
+    func set(viewModel: MessageViewModel) {
+        
+    }
+    func inactiveViewModel() {
+        self.navigationController?.popToRootViewController(animated: true)
+    }
+    
+    @IBOutlet var backButton: UIBarButtonItem!
+    
+    fileprivate let kToComposerSegue : String    = "toCompose"
+    fileprivate let kSegueMoveToFolders : String = "toMoveToFolderSegue"
+    fileprivate let kSegueToApplyLabels : String = "toApplyLabelsSegue"
+    fileprivate let kToAddContactSegue : String  = "toAddContact"
+    
+    var message: Message!
+    fileprivate var needShowShowImageView : Bool             = false
+    
+    // new code
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        UIViewController.setup(self, self.menuButton, self.shouldShowSideMenu())
+    }
+}
