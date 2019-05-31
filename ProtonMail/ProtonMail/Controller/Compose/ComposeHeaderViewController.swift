1a4a1530859706413597c06a7d9e28717effc2f7
Anatoly Rosencrantz <rosencrantz@protonmail.com>
2019-05-31 16:02:51 +0300

    revert: some minor changes for better voiceover support in composer caused layout misalignment

--- ProtonMail/ProtonMail/Controller/Compose/ComposeHeaderViewController.swift
+++ ProtonMail/ProtonMail/Controller/Compose/ComposeHeaderViewController.swift
@@ -336,11 +336,8 @@ class ComposeHeaderViewController: UIViewController {
         let _ = self.toContactPicker.becomeFirstResponder()
         UIView.animate(withDuration: self.kAnimationDuration, animations: { () -> Void in
             self.passwordView.alpha = 0.0
-            self.passwordView.isHidden = true
             self.buttonView.alpha = 0.0
-            self.buttonView.isHidden = true
             self.expirationView.alpha = 1.0
-            self.expirationView.isHidden = false
             
             self.toContactPicker.isUserInteractionEnabled = false
             self.ccContactPicker.isUserInteractionEnabled = false
@@ -519,11 +516,7 @@ class ComposeHeaderViewController: UIViewController {
         
         UIView.animate(withDuration: self.kAnimationDuration, animations: { () -> Void in
             self.expirationView.alpha = 0.0
-            self.expirationView.isHidden = true
-            self.passwordView.alpha = 1.0
-            self.passwordView.isHidden = false
             self.buttonView.alpha = 1.0
-            self.buttonView.isHidden = false
             self.delegate?.composeViewHideExpirationView(self)
         })
     }
