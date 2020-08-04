ddb2eff2f8f165f3d5630de101a9964f55393f07
Anson Chen <uvtg45@protonmail.com>
2020-08-04 15:25:06 +0800

    MAILIOS-108, The draft/sent message isn't updated if you send it from the web

--- ProtonMail/ProtonMailCommon/DataServices/MessageDataService.swift
+++ ProtonMail/ProtonMailCommon/DataServices/MessageDataService.swift
@@ -2192,6 +2192,7 @@ class MessageDataService : Service, HasLocalStorage {
                                 }
                                 
                                 messageObject.userID = self.userID
+                                messageObject.isDetailDownloaded = false
                                 
                                 if let added = msg.message?["LabelIDsAdded"] as? NSArray {
                                     for add in added {
