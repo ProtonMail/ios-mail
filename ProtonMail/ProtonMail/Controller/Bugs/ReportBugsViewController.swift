59d9af6f175cd53e53a397eef27e7a4bc4c68517
AnsonChen <uvtg45@protonmail.com>
2020-08-15 23:47:20 +0800

    MAILIOS-572, Fixed alert present when user doesn't report anything.

--- ProtonMail/ProtonMail/Controller/Bugs/ReportBugsViewController.swift
+++ ProtonMail/ProtonMail/Controller/Bugs/ReportBugsViewController.swift
@@ -34,6 +34,7 @@ class ReportBugsViewController: ProtonMailViewController {
     @IBOutlet weak var topTitleLabel: UILabel!
     
     private var kSegueToTroubleshoot : String = "toTroubleShootSegue"
+    private var reportSent: Bool = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -70,7 +71,7 @@ class ReportBugsViewController: ProtonMailViewController {
     
     override func viewDidDisappear(_ animated: Bool) {
         super.viewDidDisappear(animated)
-        guard let keywindow = UIApplication.shared.keyWindow else { return }
+        guard let keywindow = UIApplication.shared.keyWindow, self.reportSent else { return }
         keywindow.enumerateViewControllerHierarchy { (controller, stop) in
             guard controller is MenuViewController else {return}
             let alert = UIAlertController(title: LocalString._bug_report_received,
@@ -138,6 +139,7 @@ class ReportBugsViewController: ProtonMailViewController {
                 alert.addAction(UIAlertAction(title: LocalString._general_ok_action, style: .default, handler: nil))
                 self.present(alert, animated: true, completion: nil)
             } else {
+                self.reportSent = true
                 self.reset()
                 NotificationCenter.default.post(name: .switchView, object: nil)
             }
