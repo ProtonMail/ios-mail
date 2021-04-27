5fbb3f4e18918729d0d097cc5f13d7799578a37f
AnsonChen <uvtg45@protonmail.com>
2021-04-27 15:44:41 +0800

    fix update label issue

--- ProtonMail/ProtonMailCommon/ViewModels/Labels/NEWLabelEditViewModel.swift
+++ ProtonMail/ProtonMailCommon/ViewModels/Labels/NEWLabelEditViewModel.swift
@@ -366,12 +366,13 @@ extension NEWLabelEditViewModel {
                                            notify: self.notify) { [weak self] error in
             guard let self = self else { return }
             if let error = error {
+                self.uiDelegate?.hideLoadingHUD()
                 self.uiDelegate?.showAlert(message: error.localizedDescription)
                 return
             }
-            _ = self.user.labelService.fetchV4Labels().done { _ in
-                self.uiDelegate?.hideLoadingHUD()
-                self.uiDelegate?.dismiss()
+            _ = self.user.labelService.fetchV4Labels().done { [weak self] _ in
+                self?.uiDelegate?.hideLoadingHUD()
+                self?.uiDelegate?.dismiss()
             }
         }
     }
@@ -384,12 +385,15 @@ extension NEWLabelEditViewModel {
                                               parentID: self.parentID,
                                               notify: self.notify) { [weak self] _, error in
             guard let self = self else { return }
-            self.uiDelegate?.hideLoadingHUD()
             if let error = error {
+                self.uiDelegate?.hideLoadingHUD()
                 self.uiDelegate?.showAlert(message: error.localizedDescription)
                 return
             }
-            self.uiDelegate?.dismiss()
+            _ = self.user.labelService.fetchV4Labels().done { [weak self] _ in
+                self?.uiDelegate?.hideLoadingHUD()
+                self?.uiDelegate?.dismiss()
+            }
         }
     }
 }
