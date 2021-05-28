f0ac1323eda0523d36d9fbe3702c8b03573b3d7c
Mustapha Tarek BEN LECHHAB <mt.benlechhab@protonmail.com>
2021-05-28 16:05:16 +0200

    Clean up by making extension files

--- ProtonMail/ProtonMailCommon/DataServices/ConversationDataService/ConversationDataService.swift
+++ ProtonMail/ProtonMailCommon/DataServices/ConversationDataService/ConversationDataService.swift
@@ -20,8 +20,8 @@
 //  You should have received a copy of the GNU General Public License
 //  along with ProtonMail.  If not, see <https://www.gnu.org/licenses/>.
 
+import CoreData
 import Foundation
-import Groot
 import ProtonCore_Services
 
 enum ReadState {
@@ -30,6 +30,7 @@ enum ReadState {
 }
 
 protocol ConversationProvider {
+    // MARK: - Collection fetching
     func fetchConversationCounts(addressId: String?, completion: ((Result<Void, Error>) -> Void)?)
     func fetchConversations(for labelID: String,
                             before timestamp: Int,
@@ -37,21 +38,24 @@ protocol ConversationProvider {
                             shouldReset: Bool,
                             completion: ((Result<Void, Error>) -> Void)?)
     func fetchConversations(with ids: [String], completion: ((Result<Void, Error>) -> Void)?)
+    // MARK: - Single item fetching
+    // MARK: - Operations
     func deleteConversation(with id: String, completion: ((Result<Void, Error>) -> Void)?)
     func mark(conversationIDs: [String], as state: ReadState, completion: ((Result<Void, Error>) -> Void)?)
     func label(conversationIDs: [String], as label: String, completion: ((Result<Void, Error>) -> Void)?)
     func unlabel(conversationIDs: [String], as label: String, completion: ((Result<Void, Error>) -> Void)?)
+    // MARK: - Clean up
     func cleanAll()
 
 }
 
 final class ConversationDataService: Service, ConversationProvider {
-    private let apiService: APIService
-    private let userID: String
-    private let coreDataService: CoreDataService
-    private let labelDataService: LabelsDataService
-    private let lastUpdatedStore: LastUpdatedStoreProtocol
-    private weak var eventsService: EventsService?
+    let apiService: APIService
+    let userID: String
+    let coreDataService: CoreDataService
+    let labelDataService: LabelsDataService
+    let lastUpdatedStore: LastUpdatedStoreProtocol
+    private(set) weak var eventsService: EventsService?
     private weak var viewModeDataSource: ViewModeDataSource?
     private weak var queueManager: QueueManager?
 
@@ -73,201 +77,6 @@ final class ConversationDataService: Service, ConversationProvider {
         self.queueManager = queueManager
     }
 
-    func fetchConversationCounts(addressId: String?, completion: ((Result<Void, Error>) -> Void)?) {
-        let conversationCountRequest = ConversationCountRequest(addressID: addressId)
-        self.apiService.GET(conversationCountRequest) { _, response, error in
-            if let error = error {
-                completion?(.failure(error))
-                return
-            } else {
-                let countDict = response?["Counts"] as? [[String: Any]]
-                self.eventsService?.processEvents(conversationCounts: countDict)
-                completion?(.success(()))
-            }
-        }
-    }
-
-    func fetchConversations(for labelID: String,
-                            before timestamp: Int,
-                            unreadOnly: Bool,
-                            shouldReset: Bool,
-                            completion: ((Result<Void, Error>) -> Void)?) {
-        if shouldReset {
-            cleanAll()
-            lastUpdatedStore.clear()
-        }
-        var para = ConversationsRequest.Parameters()
-        if timestamp > 0 {
-            para.end = timestamp - 1
-        }
-        if unreadOnly {
-            para.unread = 1
-        }
-        para.limit = 50
-        para.sort = "Time"
-        para.desc = 1
-        para.labelID = labelID
-
-        let request = ConversationsRequest(para)
-        self.apiService.GET(request) { _, responseDict, error in
-            if let err = error {
-                DispatchQueue.main.async {
-                    completion?(.failure(err))
-                }
-                return
-            } else {
-                let response = ConversationsResponse()
-                guard response.ParseResponse(responseDict) else {
-                    let err = NSError.protonMailError(1000, localizedDescription: "Parsing error")
-                    DispatchQueue.main.async {
-                        completion?(.failure(err))
-                    }
-                    return
-                }
-
-                let messcount = responseDict?["Total"] as? Int ?? 0
-                let context = self.coreDataService.rootSavingContext
-                self.coreDataService.enqueue(context: context) { context in
-                    do {
-                        var conversationsDict = response.conversationsDict
-
-                        guard !conversationsDict.isEmpty else {
-                            DispatchQueue.main.async {
-                                completion?(.failure(NSError.protonMailError(1000, localizedDescription: "Data not found")))
-                            }
-                            return
-                        }
-
-                        for index in conversationsDict.indices {
-                            conversationsDict[index]["UserID"] = self.userID
-                            let conversationID = conversationsDict[index]["ID"]
-                            if var labels = conversationsDict[index]["Labels"] as? [[String: Any]] {
-                                for index in labels.indices {
-                                    labels[index]["UserID"] = self.userID
-                                    labels[index]["ConversationID"] = conversationID
-                                }
-                                conversationsDict[index]["Labels"] = labels
-                            }
-                        }
-
-                        if let conversations =
-                            try GRTJSONSerialization.objects(withEntityName: Conversation.Attributes.entityName,
-                                                             fromJSONArray: conversationsDict,
-                                                             in: context) as? [Conversation] {
-                            if let error = context.saveUpstreamIfNeeded() {
-                                PMLog.D(" error: \(error)")
-                            }
-                            
-                            if let lastConversation = conversations.last, let firstConversation = conversations.first {
-                                let updateTime = self.lastUpdatedStore.lastUpdateDefault(by: labelID,
-                                                                                         userID: self.userID,
-                                                                                         context: context,
-                                                                                         type: .conversation)
-                                if unreadOnly {
-                                    //Update unread query time
-                                    if updateTime.isUnreadNew {
-                                        updateTime.unreadStart = firstConversation.getTime(labelID: labelID) ?? Date()
-                                    }
-                                    if let time = lastConversation.getTime(labelID: labelID),
-                                       (updateTime.unreadEndTime.compare(time) == .orderedDescending)
-                                        || updateTime.unreadEndTime == .distantPast {
-                                        updateTime.unreadEnd = time
-                                    }
-                                } else {
-                                    // Update normal query time
-                                    if updateTime.isNew {
-                                        updateTime.start = firstConversation.getTime(labelID: labelID) ?? Date()
-                                        updateTime.total = Int32(messcount)
-                                    }
-                                    if let time = lastConversation.getTime(labelID: labelID),
-                                       (updateTime.unreadEndTime.compare(time) == .orderedDescending)
-                                        || updateTime.unreadEndTime == .distantPast {
-                                        updateTime.end = time
-                                    }
-                                    updateTime.update = Date()
-                                }
-                            }
-                        }
-                        if shouldReset {
-                            _ = self.labelDataService.fetchV4Labels()
-                        }
-                        DispatchQueue.main.async {
-                            completion?(.success(()))
-                        }
-                    } catch {
-                        PMLog.D("error: \(error)")
-                        DispatchQueue.main.async {
-                            completion?(.failure(error))
-                        }
-                    }
-                }
-            }
-        }
-    }
-
-    func fetchConversations(with ids: [String], completion: ((Result<Void, Error>) -> Void)?) {
-        var para = ConversationsRequest.Parameters()
-        para.IDs = ids
-        
-        let request = ConversationsRequest(para)
-        self.apiService.GET(request) { (task, responseDict, error) in
-            if let err = error {
-                DispatchQueue.main.async {
-                    completion?(.failure(err))
-                }
-            } else {
-                let response = ConversationsResponse()
-                guard response.ParseResponse(responseDict) else {
-                    let err = NSError.protonMailError(1000, localizedDescription: "Parsing error")
-                    DispatchQueue.main.async {
-                        completion?(.failure(err))
-                    }
-                    return
-                }
-                
-                let context = self.coreDataService.rootSavingContext
-                self.coreDataService.enqueue(context: context) { (context) in
-                    do {
-                        var conversationsDict = response.conversationsDict
-                        
-                        guard !conversationsDict.isEmpty else {
-                            DispatchQueue.main.async {
-                                completion?(.failure(NSError.protonMailError(1000, localizedDescription: "Data not found")))
-                            }
-                            return
-                        }
-                        
-                        for index in conversationsDict.indices {
-                            conversationsDict[index]["UserID"] = self.userID
-                            let conversationID = conversationsDict[index]["ID"]
-                            if var labels = conversationsDict[index]["Labels"] as? [[String: Any]] {
-                                for (index, _) in labels.enumerated() {
-                                    labels[index]["UserID"] = self.userID
-                                    labels[index]["ConversationID"] = conversationID
-                                }
-                                conversationsDict[index]["Labels"] = labels
-                            }
-                        }
-                        
-                        if (try GRTJSONSerialization.objects(withEntityName: Conversation.Attributes.entityName, fromJSONArray: conversationsDict, in: context) as? [Conversation]) != nil {
-                            if let error = context.saveUpstreamIfNeeded() {
-                                PMLog.D(" error: \(error)")
-                            }
-                        }
-                        DispatchQueue.main.async {
-                            completion?(.success(()))
-                        }
-                    } catch {
-                        PMLog.D("error: \(error)")
-                        DispatchQueue.main.async {
-                            completion?(.failure(error as NSError))
-                        }
-                    }
-                }
-            }
-        }
-    }
-
     func deleteConversation(with id: String, completion: ((Result<Void, Error>) -> Void)?) {
         fatalError("Not implemented")
     }
@@ -285,6 +94,7 @@ final class ConversationDataService: Service, ConversationProvider {
     }
 }
 
+// MARK: - Clean up
 extension ConversationDataService {
     func cleanAll() {
         let context = coreDataService.mainContext
