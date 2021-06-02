6f20185f51031b05d6902416d4fcd6cbf47681ea
Steven Lin <steven84059@pm.me>
2021-06-02 16:47:48 +0800

    Remove unnecessary logs and commented code

--- ProtonMail/ProtonMailCommon/Encryption/OpenPGP+Extension.swift
+++ ProtonMail/ProtonMailCommon/Encryption/OpenPGP+Extension.swift
@@ -178,7 +178,7 @@ extension Data {
             do {
                 if let token = key.token, let signature = key.signature { //have both means new schema. key is
                     if let plaitToken = try token.decryptMessage(binKeys: userKeys, passphrase: passphrase) {
-                        PMLog.D(signature)
+//                        PMLog.D(signature)
                         return try Crypto().decryptAttachment(keyPacket: keyPackage,
                                                               dataPacket: self,
                                                               privateKey: key.privateKey,
@@ -251,7 +251,7 @@ extension Data {
             do {
                 if let token = key.token, let signature = key.signature { //have both means new schema. key is
                     if let plainToken = try token.decryptMessage(binKeys: userKeys, passphrase: passphrase) {
-                        PMLog.D(signature)
+//                        PMLog.D(signature)
                         return try Crypto().getSession(keyPacket: self, privateKey: key.privateKey, passphrase: plainToken)
                     }
                 } else if let token = key.token { //old schema with token - subuser. key is embed singed
--- ProtonMail/ProtonMailCommon/Network/APIService+MessagesExtension.swift
+++ ProtonMail/ProtonMailCommon/Network/APIService+MessagesExtension.swift
@@ -33,53 +33,7 @@ fileprivate struct MessagePath {
 //TODO :: all the request post put ... all could abstract a body layer, after change the request only need the url and request object.
 /// Messages extension
 extension APIService {
-    
-//    enum Filter: Int {
-//        case noFilter = -2
-//        case read = 0
-//        case unRead = 1
-//    }
-//
-//    enum Order: Int {
-//        case ascending = 0
-//        case descending = 1
-//    }
-//
-//    enum SortedColumn: String {
-//        case date = "Date"
-//        case from = "From"
-//        case size = "Size"
-//        case subject = "Subject"
-//    }
-//
-//    struct MessageErrorCode {
-//        static let credentialExpired = 10
-//        static let credentialInvalid = 20
-//        static let invalidGrant = 30
-//        static let unableToParseToken = 40
-//    }
-//
-//    //new way to do the new work calls
-//    func POST<T> ( _ apiRequest : ApiRequest<T>!, authCredential: AuthCredential? = nil, completion: CompletionBlock?) {
-//        let parameterStrings = apiRequest.toDictionary()
-//        request(method: .post,
-//                path: apiRequest.path(),
-//                parameters: parameterStrings,
-//                headers: self.getHeader(apiRequest),
-//                customAuthCredential: authCredential,
-//                completion: completion)
-//    }
-//
-//    func PUT<T> ( _ apiRequest : ApiRequest<T>!, authCredential: AuthCredential? = nil, completion: CompletionBlock?) {
-//        let parameterStrings = apiRequest.toDictionary()
-//        request(method: .put,
-//                path: apiRequest.path(),
-//                parameters: parameterStrings,
-//                headers: self.getHeader(apiRequest),
-//                customAuthCredential: authCredential,
-//                completion: completion)
-//    }
-//
+
     func GET( _ api : Request, authCredential: AuthCredential? = nil, completion: CompletionBlock?) {
         var headers = api.header
         headers[HTTPHeader.apiVersion] = api.version
@@ -92,61 +46,9 @@ extension APIService {
                      customAuthCredential: api.authCredential,
                      completion: completion)
     }
-//
-//    func Delete<T> ( _ apiRequest : ApiRequest<T>!, authCredential: AuthCredential? = nil, completion: CompletionBlock?) {
-//        let parameterStrings = apiRequest.toDictionary()
-//        request(method: .delete,
-//                path: apiRequest.path(),
-//                parameters: parameterStrings,
-//                headers: self.getHeader(apiRequest),
-//                customAuthCredential: authCredential,
-//                completion: completion)
-//    }
-//
-//    private func getHeader<T>(_ apiRequest : ApiRequest<T>) -> [String: Int] {
-//        if apiRequest.ver() == -1 {
-//            return [:]
-//        }
-//        return
-//    }
-//
-//    // MARK : Need change soon tempry for no outside incoming emails
-//    func fetchLatestMessageList(_ time: Int, completion: @escaping CompletionBlock) {
-//        let path = MessagePath.base + "/latest/\(time)"
-//        request(method: .get,
-//                path: path,
-//                parameters: nil,
-//                headers: [:],
-//                completion: completion)
-//    }
-//
-//
-//    // MARK: - Public methods
-//    func messageCheck(timestamp: TimeInterval, completion: CompletionBlock?) {
-//        let path = MessagePath.base + "/check"
-//        let parameters = ["t" : timestamp]
-//        request(method: .get,
-//                path: path,
-//                parameters: parameters,
-//                headers: [:],
-//                completion: completion)
-//    }
-//
-//    func messageCountForLocation(_ location: Int, completion: CompletionBlock?) {
-//        let path = MessagePath.base
-//        let parameters = ["Location" : location]
-//        let completionWrapper = completionWrapperParseCompletion(completion, forKey: "MessageCount")
-//        request(method: .get,
-//                path: path,
-//                parameters: parameters,
-//                headers: [:],
-//                completion: completionWrapper)
-//    }
-//
-//
+
     func messageDetail(messageID: String, completion: @escaping CompletionBlock) {
         let path = MessagePath.base + "/\(messageID)"
-        PMLog.D("path: \(path)")
         self.request(method: .get,
                      path: path,
                      parameters: nil,
@@ -156,30 +58,4 @@ extension APIService {
                      customAuthCredential: nil,
                      completion: completion)
     }
-//
-//    func messageSearch(_ query: String, page: Int, completion: CompletionBlock?) {
-//        let path = MessagePath.base
-//        let parameters :  [String : Any] = [
-//            "Keyword" : query,
-//            "Page" : page
-//        ]
-//
-//        request(method: .get,
-//                path: path,
-//                parameters: parameters,
-//                headers: [:],
-//                completion: completion)
-//    }
-//
-//
-//    // MARK: - Private methods
-//    fileprivate func filteredMessageStringParameters(_ parameters: [String : String]) -> [String : String] {
-//        var filteredParameters: [String : String] = [:]
-//        for (key, value) in parameters {
-//            if !value.isEmpty {
-//                filteredParameters[key] = value
-//            }
-//        }
-//        return filteredParameters
-//    }
 }
