0d8f6311274727028f9f8c3c47577f43f1390b31
AnsonChen <uvtg45@protonmail.com>
2021-06-07 13:49:54 +0800

    MAILIOS-1300, The message stays in the Draft if you restart the app while sending even though is sent

--- ProtonMail/ProtonMailCommon/DataServices/MessageDataService/MessageDataService.swift
+++ ProtonMail/ProtonMailCommon/DataServices/MessageDataService/MessageDataService.swift
@@ -1404,6 +1404,9 @@ class MessageDataService : Service, HasLocalStorage {
                     // Since the message is sent, this alert is useless to user
                     self.localNotificationService.unscheduleMessageSendingFailedNotification(.init(messageID: message.messageID))
                     completion?(nil, nil, nil)
+                    // Draft folder must be single message mode
+                    let draftID = Message.Location.draft.rawValue
+                    self.fetchMessagesWithReset(byLabel: draftID, time: 0, cleanContact: false, completion: nil)
                     return
                 } else {
                     NSError.alertMessageSentError(details: err.localizedDescription)
