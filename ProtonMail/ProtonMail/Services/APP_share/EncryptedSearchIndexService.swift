73ceb6c977425fe8b9aa89de0ade00b319ce4ff9
Ralph Ankele <ralph.ankele@protonmail.com>
2021-07-19 16:45:15 +0200

    create database structure for search index

--- ProtonMail/ProtonMail/Services/APP_share/EncryptedSearchIndexService.swift
+++ ProtonMail/ProtonMail/Services/APP_share/EncryptedSearchIndexService.swift
@@ -35,4 +35,62 @@ extension EncryptedSearchIndexService {
         }
         return self.handleToSQliteDB
     }
+    
+    enum DatabaseConstants {
+        static let Table_Searchable_Messages = "SearchableMessage"
+        static let Column_Searchable_Message_Id = "ID"
+        static let Column_Searchable_Message_Time = "Time"
+        static let Column_Searchable_Message_Refresh_Bit = "RefreshBit"
+        static let Column_Searchable_Message_Labels = "LabelIDs"
+        static let Column_Searchable_Message_Has_Body = "HasBody"
+        static let Column_Searchable_Message_Is_Starred = "IsStarred"
+        static let Column_Searchable_Message_Unread = "Unread"
+        static let Column_Searchable_Message_Location = "Location"
+        static let Column_Searchable_Message_Order = "MessageOrder"
+        static let Column_Searchable_Message_Decryption_Failed = "DecryptionFailed"
+        
+        static let Column_Searchable_Message_Encrypted_Content = "EncryptedContent"
+        static let Column_Searchable_Message_Encrypted_Content_File = "EncryptedContentFile"
+        static let Column_Searchable_Message_Encryption_IV = "EncryptionIV"
+    }
+    
+    func createSearchIndexTable() -> Void {
+        let messages = Table(DatabaseConstants.Table_Searchable_Messages)
+        let id = Expression<String>(DatabaseConstants.Column_Searchable_Message_Id)
+        let time = Expression<CLong>(DatabaseConstants.Column_Searchable_Message_Time)
+        let refreshBit = Expression<Bool>(DatabaseConstants.Column_Searchable_Message_Refresh_Bit)
+        //let labels = Expression<[String]>(DatabaseConstants.Column_Searchable_Message_Labels)
+        let labels = Expression<String>(DatabaseConstants.Column_Searchable_Message_Labels)
+        let hasBody = Expression<Bool>(DatabaseConstants.Column_Searchable_Message_Has_Body)
+        let isStarred = Expression<Bool?>(DatabaseConstants.Column_Searchable_Message_Is_Starred)
+        let unread = Expression<Bool>(DatabaseConstants.Column_Searchable_Message_Unread)
+        let location = Expression<Int>(DatabaseConstants.Column_Searchable_Message_Location)
+        let order = Expression<CLong?>(DatabaseConstants.Column_Searchable_Message_Order)
+        let decryptionFailed = Expression<Bool>(DatabaseConstants.Column_Searchable_Message_Decryption_Failed)
+        
+        let encryptedContent = Expression<String?>(DatabaseConstants.Column_Searchable_Message_Encrypted_Content)
+        let encryptedContentFile = Expression<String?>(DatabaseConstants.Column_Searchable_Message_Encrypted_Content_File)
+        let encryptionIV = Expression<String?>(DatabaseConstants.Column_Searchable_Message_Encryption_IV)
+        
+        do {
+            try self.handleToSQliteDB?.run(messages.create(ifNotExists: true) {
+                t in
+                t.column(id, primaryKey: true)  //TODO set default value
+                t.column(time, defaultValue: 0)
+                t.column(labels)    //TODO set default value
+                t.column(isStarred, defaultValue: nil)
+                t.column(unread, defaultValue: false)
+                t.column(location, defaultValue: -1)
+                t.column(order, defaultValue: nil)
+                t.column(refreshBit, defaultValue: false)
+                t.column(hasBody, defaultValue: false)
+                t.column(decryptionFailed, defaultValue: false)
+                t.column(encryptionIV, defaultValue: nil)
+                t.column(encryptedContent, defaultValue: nil)
+                t.column(encryptedContentFile, defaultValue: nil)
+            })
+        } catch {
+            print("Unexpected error: \(error).")
+        }
+    }
 }
