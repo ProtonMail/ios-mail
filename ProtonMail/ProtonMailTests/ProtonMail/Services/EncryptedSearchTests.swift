3f2768d4113ee8d5f4bcb0656b877fc15e5641b0
Ralph Ankele <ralph.ankele@protonmail.com>
2021-08-27 13:14:37 +0200

    fix bug when extracting content from HTML emails - add some unit tests

--- ProtonMail/ProtonMailTests/ProtonMail/Services/EncryptedSearchTests.swift
+++ ProtonMail/ProtonMailTests/ProtonMail/Services/EncryptedSearchTests.swift
@@ -28,9 +28,9 @@ class EncryptedSearchTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testBuildSearchIndex() throws {
+    /*func testBuildSearchIndex() throws {
         //TODO
-    }
+    }*/
     
     func testExtractKeyWordsFromBody() throws {
         let body: String = "<div dir=\"ltr\">this is a testmessage<br></div>\n"
@@ -38,6 +38,83 @@ class EncryptedSearchTests: XCTestCase {
         let result = sut(body, true)
         
         XCTAssertEqual(result, "this is a testmessage")
+    }
+    
+    func testExtractKeyWordsFromBodyRemoveBlockQuotesProtonMail(){
+        let body: String = """
+            <div>This a reply<br></div><div><br></div><div class="protonmail_signature_block"><div class="protonmail_signature_block-user protonmail_signature_block-empty"></div><div class="protonmail_signature_block-proton">Sent with <a href="https://protonmail.com/" target="_blank">ProtonMail</a> Secure Email.</div></div><div><br></div><div class="protonmail_quote">
+            ‐‐‐‐‐‐‐ Original Message ‐‐‐‐‐‐‐<br>
+            On Wednesday, April 28th, 2021 at 6:33 PM, marin.test &lt;marin.test@protonmail.com&gt; wrote:<br>
+            <blockquote class="protonmail_quote" type="cite">
+                <div>Some random word: salon<br></div><div><br></div><div class="protonmail_signature_block"><div class="protonmail_signature_block-user protonmail_signature_block-empty"></div><div class="protonmail_signature_block-proton">Sent with <a target="_blank" href="https://protonmail.com/" rel="noreferrer nofollow noopener">ProtonMail</a> Secure Email.</div></div><div><br></div>
+            </blockquote><br>
+        </div>
+        """.preg_replace("\\s+", replaceto: " ").trimmingCharacters(in: .whitespacesAndNewlines)
+        let bodyCleanedExpected: String = """
+            This a reply
+            
+            Sent with ProtonMail Secure Email.
+        """.preg_replace("\\s+", replaceto: " ").trimmingCharacters(in: .whitespacesAndNewlines)
+        let sut = EncryptedSearchService.shared.extractKeywordsFromBody
+        let result = sut(body, true)
+        
+        XCTAssertEqual(result, bodyCleanedExpected)
+    }
+    
+    func testExtractKeyWordsFromBodyLeaveBlockQuotesProtonMail(){
+        let body: String = """
+            <div>This a reply<br></div><div><br></div><div class="protonmail_signature_block"><div class="protonmail_signature_block-user protonmail_signature_block-empty"></div><div class="protonmail_signature_block-proton">Sent with <a href="https://protonmail.com/" target="_blank">ProtonMail</a> Secure Email.</div></div><div><br></div><div class="protonmail_quote">
+            ‐‐‐‐‐‐‐ Original Message ‐‐‐‐‐‐‐<br>
+            On Wednesday, April 28th, 2021 at 6:33 PM, marin.test &lt;marin.test@protonmail.com&gt; wrote:<br>
+            <blockquote class="protonmail_quote" type="cite">
+                <div>Some random word: salon<br></div><div><br></div><div class="protonmail_signature_block"><div class="protonmail_signature_block-user protonmail_signature_block-empty"></div><div class="protonmail_signature_block-proton">Sent with <a target="_blank" href="https://protonmail.com/" rel="noreferrer nofollow noopener">ProtonMail</a> Secure Email.</div></div><div><br></div>
+            </blockquote><br>
+        </div>
+        """.preg_replace("\\s+", replaceto: " ").trimmingCharacters(in: .whitespacesAndNewlines)
+        let bodyCleanedExpected: String = """
+            This a reply
+
+            Sent with ProtonMail Secure Email.
+            ‐‐‐‐‐‐‐ Original Message ‐‐‐‐‐‐‐
+            On Wednesday, April 28th, 2021 at 6:33 PM, marin.test <marin.test@protonmail.com> wrote:
+            Some random word: salon
+            
+            Sent with ProtonMail Secure Email.
+        """.preg_replace("\\s+", replaceto: " ").trimmingCharacters(in: .whitespacesAndNewlines)
+        let sut = EncryptedSearchService.shared.extractKeywordsFromBody
+        let result = sut(body, false)
+        
+        XCTAssertEqual(result, bodyCleanedExpected)
+    }
+    
+    func testExtractKeyWordsFromBodyRemoveBlockQuotesGMail(){
+        let body: String = """
+        <div dir="ltr">This is a reply<br></div><br><div class="gmail_quote"><div dir="ltr" class="gmail_attr">On Wed, Apr 28, 2021 at 6:38 PM marin.test &lt;<a href="mailto:marin.test@protonmail.com">marin.test@protonmail.com</a>&gt; wrote:<br></div><blockquote class="gmail_quote" style="margin:0px 0px 0px 0.8ex;border-left:1px solid rgb(204,204,204);padding-left:1ex"><div>Some random word: peasant<br></div><div><br></div><div><div></div><div>Sent with <a href="https://protonmail.com/" target="_blank">ProtonMail</a> Secure Email.</div></div></blockquote></div>
+        """.preg_replace("\\s+", replaceto: " ").trimmingCharacters(in: .whitespacesAndNewlines)
+        let bodyCleanedExpected: String = "This is a reply".preg_replace("\\s+", replaceto: " ").trimmingCharacters(in: .whitespacesAndNewlines)
+        let sut = EncryptedSearchService.shared.extractKeywordsFromBody
+        let result = sut(body, true)
+        
+        XCTAssertEqual(result, bodyCleanedExpected)
+    }
+    
+    func testExtractKeyWordsFromBodyLeaveBlockQuotesGMail(){
+        let body: String = """
+        <div dir="ltr">This is a reply<br></div><br><div class="gmail_quote"><div dir="ltr" class="gmail_attr">On Wed, Apr 28, 2021 at 6:38 PM marin.test &lt;<a href="mailto:marin.test@protonmail.com">marin.test@protonmail.com</a>&gt; wrote:<br></div><blockquote class="gmail_quote" style="margin:0px 0px 0px 0.8ex;border-left:1px solid rgb(204,204,204);padding-left:1ex"><div>Some random word: peasant<br></div><div><br></div><div><div></div><div>Sent with <a href="https://protonmail.com/" target="_blank">ProtonMail</a> Secure Email.</div></div></blockquote></div>
+        """.preg_replace("\\s+", replaceto: " ").trimmingCharacters(in: .whitespacesAndNewlines)
+        let bodyCleanedExpected: String = """
+            This is a reply
+            
+            On Wed, Apr 28, 2021 at 6:38 PM marin.test <marin.test@protonmail.com> wrote:
+            
+            Some random word: peasant
+            
+            Sent with ProtonMail Secure Email.
+        """.preg_replace("\\s+", replaceto: " ").trimmingCharacters(in: .whitespacesAndNewlines)
+        let sut = EncryptedSearchService.shared.extractKeywordsFromBody
+        let result = sut(body, false)
+        
+        XCTAssertEqual(result, bodyCleanedExpected)
     }
 
 }
