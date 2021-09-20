fa9f7acab1649b21cc6b59e43d145fd06df01478
Ralph Ankele <ralph.ankele@protonmail.com>
2021-09-20 12:43:46 +0200

    fix warning due to unused variable

--- ProtonMail/ProtonMail/Services/APP_share/EncryptedSearchService.swift
+++ ProtonMail/ProtonMail/Services/APP_share/EncryptedSearchService.swift
@@ -1272,7 +1272,7 @@ extension EncryptedSearchService {
     func getTotalAvailableMemory() -> Double {
         var taskInfo = task_vm_info_data_t()
         var count = mach_msg_type_number_t(MemoryLayout<task_vm_info>.size) / 4
-        let re_kern_return_t = withUnsafeMutablePointer(to: &taskInfo) {
+        let _ = withUnsafeMutablePointer(to: &taskInfo) {
             $0.withMemoryRebound(to: integer_t.self, capacity: 1) {
                 task_info(mach_task_self_, task_flavor_t(TASK_VM_INFO), $0, &count)
                 }
