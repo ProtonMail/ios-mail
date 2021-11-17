a2f75992c76c4f0ccb460ff01e0b30e852f4a8be
Ralph Ankele <ralph.ankele@proton.ch>
2021-11-17 10:14:50 +0100

    add some more unit tests for EncryptedSearchCacheService

--- ProtonMail/ProtonMailTests/ProtonMail/Services/EncryptedSearchCacheServiceTests.swift
+++ ProtonMail/ProtonMailTests/ProtonMail/Services/EncryptedSearchCacheServiceTests.swift
@@ -24,23 +24,39 @@ class EncryptedSearchCacheServiceTests: XCTestCase {
     var testUserID: String!
     var connectionToSearchIndexDB: Connection!
     var testSearchIndexDBName: String!
+    var testCache: EncryptedsearchCache!
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
 
         self.testUserID = "test"
-        self.testSearchIndexDBName = "encryptedSearchIndex_test.sqlite3"
 
         // Create a search index db for user 'test'.
+        self.createTestSearchIndexDB()
+
+        //build the cache for user 'test'
+        self.buildTestCache()
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+
+        // delete search index db for user 'test'
+        try self.deleteTestSearchIndexDB()
+
+        // delete cache for user 'test'
+        EncryptedSearchCacheService.shared.deleteCache(userID: self.testUserID)
+    }
+
+    func createTestSearchIndexDB(){
+        self.testSearchIndexDBName = "encryptedSearchIndex_test.sqlite3"
         self.connectionToSearchIndexDB = EncryptedSearchIndexService.shared.connectToSearchIndex(for: self.testUserID)!
         EncryptedSearchIndexService.shared.createSearchIndexTable(using: self.connectionToSearchIndexDB)
         _ = EncryptedSearchIndexService.shared.addNewEntryToSearchIndex(for: self.testUserID, messageID: "uniqueID1", time: 1637058775, labelIDs: ["5", "1"], isStarred: false, unread: false, location: 1, order: 1, hasBody: true, decryptionFailed: false, encryptionIV: Data("iv".utf8), encryptedContent: Data("content".utf8), encryptedContentFile: "linktofile")
         _ = EncryptedSearchIndexService.shared.addNewEntryToSearchIndex(for: self.testUserID, messageID: "uniqueID2", time: 1637141557, labelIDs: ["5", "1"], isStarred: false, unread: false, location: 1, order: 2, hasBody: true, decryptionFailed: false, encryptionIV: Data("iv".utf8), encryptedContent: Data("content".utf8), encryptedContentFile: "linktofile")
     }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        
+    
+    func deleteTestSearchIndexDB() throws {
         // Create the path to the database for user 'test'.
         let pathToTestDB: String = EncryptedSearchIndexService.shared.getSearchIndexPathToDB(self.testSearchIndexDBName)
         let urlToDB: URL? = URL(string: pathToTestDB)
@@ -50,7 +66,15 @@ class EncryptedSearchCacheServiceTests: XCTestCase {
         self.connectionToSearchIndexDB = nil
 
         // Remove the database file.
-        //try FileManager.default.removeItem(atPath: urlToDB!.path)
+        try FileManager.default.removeItem(atPath: urlToDB!.path)
+    }
+
+    func buildTestCache(){
+        let dbParams = EncryptedSearchIndexService.shared.getDBParams(self.testUserID)
+        var error: NSError?
+        let testKey = CryptoRandomToken(32, &error)
+        let cipher = EncryptedsearchAESGCMCipher(testKey)
+        self.testCache = EncryptedSearchCacheService.shared.buildCacheForUser(userId: self.testUserID, dbParams: dbParams, cipher: cipher!)
     }
 
     func testEncryptedSearchCacheServiceSingleton() throws {
@@ -72,18 +96,29 @@ class EncryptedSearchCacheServiceTests: XCTestCase {
     }
 
     func testDeleteCache() throws {
-        //TODO
+        let sut = EncryptedSearchCacheService.shared.deleteCache
+        sut(self.testUserID)
+
+        let isCacheStillExisting: Bool = EncryptedSearchCacheService.shared.isCacheBuilt(userID: self.testUserID)
+        XCTAssertEqual(isCacheStillExisting, false) // Cache should not exist anymore.
     }
 
-    func testUpdateCachedMessage() throws {
+    //func testUpdateCachedMessage() throws {
         //TODO
-    }
+    //}
 
-    func testDeleteCachedMessage() throws {
+    //func testDeleteCachedMessage() throws {
         //TODO
-    }
+    //}
 
     func testIsCacheBuilt() throws {
-        //TODO
+        let sut = EncryptedSearchCacheService.shared.isCacheBuilt
+        let userIDNotExisting: String = "abc"
+        let resultFalse: Bool = sut(userIDNotExisting)
+        XCTAssertFalse(resultFalse)
+
+        self.buildTestCache()
+        let resultTrue: Bool = sut(self.testUserID)
+        XCTAssertTrue(resultTrue)
     }
 }
