11ce49f703234815628a29e5ce6396a44f49e90c
Ralph Ankele <ralph.ankele@proton.ch>
2021-11-23 12:16:45 +0100

    some UI changes

--- ProtonMail/ProtonMail/Controller/Settings/Cells/ProgressBarButtonTableViewCell.swift
+++ ProtonMail/ProtonMail/Controller/Settings/Cells/ProgressBarButtonTableViewCell.swift
@@ -28,9 +28,66 @@ import UIKit
     
     override func awakeFromNib() {
         super.awakeFromNib()
-        //TODO some UI changes to progress view and button?
-        self.pauseButton.setMode(mode: .solid)
-        self.pauseButton.setTitle("Pause", for: UIControl.State.normal)
+        
+        self.pauseButton.setTitle(LocalString._encrypted_search_pause_button, for: UIControl.State.normal)
+        self.pauseButton.titleLabel?.font = UIFont.systemFont(ofSize: 13)
+        self.pauseButton.setTitleColor(ColorProvider.TextNorm, for: .normal)
+        self.pauseButton.translatesAutoresizingMaskIntoConstraints = false
+        self.pauseButton.backgroundColor = UIColor(hex: 0xEAE7E4, alpha: 1) //TODO replace with ColorProvider
+        self.pauseButton.layer.cornerRadius = 8
+        NSLayoutConstraint.activate([
+            self.pauseButton.topAnchor.constraint(equalTo: self.contentView.topAnchor, constant: 108),
+            self.pauseButton.bottomAnchor.constraint(equalTo: self.contentView.bottomAnchor, constant: -16),
+            self.pauseButton.widthAnchor.constraint(equalToConstant: 69),
+            self.pauseButton.heightAnchor.constraint(equalToConstant: 32)
+        ])
+        
+        self.progressView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            self.progressView.topAnchor.constraint(equalTo: self.contentView.topAnchor, constant: 60),
+            self.progressView.bottomAnchor.constraint(equalTo: self.contentView.bottomAnchor, constant: -88),
+            self.progressView.widthAnchor.constraint(equalToConstant: 343),
+            self.progressView.heightAnchor.constraint(equalToConstant: 8)
+        ])
+        
+        self.estimatedTimeLabel.translatesAutoresizingMaskIntoConstraints = false
+        self.estimatedTimeLabel.text = LocalString._encrypted_search_default_text_estimated_time_label
+        
+        NSLayoutConstraint.activate([
+            self.estimatedTimeLabel.topAnchor.constraint(equalTo: self.contentView.topAnchor, constant: 76),
+            self.estimatedTimeLabel.bottomAnchor.constraint(equalTo: self.contentView.bottomAnchor, constant: -64),
+            self.estimatedTimeLabel.widthAnchor.constraint(equalToConstant: 343),
+            self.estimatedTimeLabel.heightAnchor.constraint(equalToConstant: 16)
+        ])
+
+        self.currentProgressLabel.translatesAutoresizingMaskIntoConstraints = false
+        self.currentProgressLabel.textAlignment = .right
+        NSLayoutConstraint.activate([
+            self.currentProgressLabel.topAnchor.constraint(equalTo: self.contentView.topAnchor, constant: 76),
+            self.currentProgressLabel.bottomAnchor.constraint(equalTo: self.contentView.bottomAnchor, constant: -64),
+            self.currentProgressLabel.widthAnchor.constraint(equalToConstant: 343),
+            self.currentProgressLabel.heightAnchor.constraint(equalToConstant: 16)
+        ])
+
+        //status label is hidden by default
+        self.statusLabel.translatesAutoresizingMaskIntoConstraints = false
+        self.statusLabel.textAlignment = .left
+        self.statusLabel.isHidden = true
+        NSLayoutConstraint.activate([
+            self.statusLabel.topAnchor.constraint(equalTo: self.contentView.topAnchor, constant: 108),
+            self.statusLabel.bottomAnchor.constraint(equalTo: self.contentView.bottomAnchor, constant: -16),
+            self.statusLabel.widthAnchor.constraint(equalToConstant: 343),
+            self.statusLabel.heightAnchor.constraint(equalToConstant: 16)
+        ])
+        
+        self.titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        self.titleLabel.textColor = ColorProvider.TextNorm
+        NSLayoutConstraint.activate([
+            self.titleLabel.topAnchor.constraint(equalTo: self.contentView.topAnchor, constant: 12),
+            self.titleLabel.bottomAnchor.constraint(equalTo: self.contentView.bottomAnchor, constant: -120),
+            self.titleLabel.widthAnchor.constraint(equalToConstant: 289.7),
+            self.titleLabel.heightAnchor.constraint(equalToConstant: 24)
+        ])
     }
     
     @IBOutlet weak var progressView: UIProgressView!
@@ -38,19 +95,19 @@ import UIKit
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var estimatedTimeLabel: UILabel!
     @IBOutlet weak var currentProgressLabel: UILabel!
-    @IBOutlet weak var pauseButton: ProtonButton!
+    @IBOutlet weak var pauseButton: UIButton!
     
     @IBAction func pauseButtonPressed(_ sender: UIButton) {
         callback?()
+        self.layoutIfNeeded()
     }
     
-    func configCell(_ titleLine: String, _ topLine: String, _ estimatedTime: Int, _ currentProgress: Int, _ textButtonNormal: String, _ textButtonPressed: String, complete: buttonActionBlock?) {
-        
+    func configCell(_ titleLine: String, _ status: String, _ estimatedTime: Int, _ currentProgress: Int, complete: buttonActionBlock?) {
         var leftAttributes = FontManager.Default.alignment(.left)
-        leftAttributes[.foregroundColor] = ColorProvider.InteractionNorm
+        leftAttributes[.foregroundColor] = ColorProvider.TextNorm
         titleLabel.attributedText = NSMutableAttributedString(string: titleLine, attributes: leftAttributes)
         
-        statusLabel.text = topLine
+        statusLabel.text = status
         estimatedTimeLabel.text = String(estimatedTime) + " minutes remaining..."
         currentProgressLabel.text = String(currentProgress) + "%"
         progressView.setProgress(Float(currentProgress)/100.0, animated: true)
