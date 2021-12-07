2e12ca9e6d50490ad6b1bb4893fb0c3943501e16
Ralph Ankele <ralph.ankele@proton.ch>
2021-12-07 17:14:12 +0100

    correctly set up test user and test message for unit tests

--- ProtonMail/ProtonMailTests/ProtonMail/Services/EncryptedSearchTests.swift
+++ ProtonMail/ProtonMailTests/ProtonMail/Services/EncryptedSearchTests.swift
@@ -18,6 +18,14 @@
 import XCTest
 import Crypto
 import SQLite
+import CoreData
+import Groot
+
+import ProtonCore_Doh
+import ProtonCore_Services
+import ProtonCore_Networking
+import ProtonCore_DataModel
+
 @testable import ProtonMail
 
 class EncryptedSearchTests: XCTestCase {
@@ -26,9 +34,16 @@ class EncryptedSearchTests: XCTestCase {
     var testSearchIndexDBName: String!
     var connectionToSearchIndexDB: Connection!
 
+    var coreDataService: CoreDataService!
+    var testContext: NSManagedObjectContext!
+    let customLabelId = "Vg_DqN6s-xg488vZQBkiNGz0U-62GKN6jMYRnloXY-isM9s5ZR-rWCs_w8k9Dtcc-sVC-qnf8w301Q-1sA6dyw=="
+
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
-        self.testUserID = "test"
+
+        self.testUserID = self.setUpTestUser()!
+        print("Test user id: \(self.testUserID)")
+
         self.testMessageID = "uniqueID1"
 
         // Create a test search index for user 'test'
@@ -37,6 +52,9 @@ class EncryptedSearchTests: XCTestCase {
         print("Test database created: \(doesTestIndexExist)")
         let numberOfEntries = EncryptedSearchIndexService.shared.getNumberOfEntriesInSearchIndex(for: self.testUserID)
         print("Entries in db: \(numberOfEntries)")
+        
+        // Set up core data to create some test messages
+        try self.setupCoreData()
     }
 
     override func tearDownWithError() throws {
@@ -48,10 +66,23 @@ class EncryptedSearchTests: XCTestCase {
         // Reset some values in EncryptedSearchService Singleton
         EncryptedSearchService.shared.numInterruptions = 0
         EncryptedSearchService.shared.numPauses = 0
+
+        // Delete core data
+        self.deleteCoreData()
     }
 
-    func createTestSearchIndexDB(){
-        self.testSearchIndexDBName = "encryptedSearchIndex_test.sqlite3"
+    private func setUpTestUser() -> String? {
+        let users: UsersManager = sharedServices.get(by: UsersManager.self)
+        let testUserInfo = UserInfo(displayName: "test display name", maxSpace: 42, notificationEmail: "test notification name",signature: "test signature", usedSpace: 123, userAddresses: [], autoSC: 321, language: "DE", maxUpload: 234, notify: 2345, showImage: 645, swipeL: 3452, swipeR: 4132, role: 1234, delinquent: 4123, keys: [], userId: "test", sign: 1234, attachPublicKey: 5467, linkConfirmation: "test link confirmation", credit: 098, currency: "BOL", pwdMode: 667, twoFA: 776, enableFolderColor: 77, inheritParentFolderColor: 88, subscribed: 12, groupingMode: 1, weekStart: 0)
+        let testAuth = AuthCredential(sessionID: "test session id", accessToken: "test access token", refreshToken: "test refresh token",expiration: .distantFuture, userName: "test user name", userID: "test", privateKey: "test private key", passwordKeySalt: "test password key salt")
+        let apiService = PMAPIService(doh: users.doh, sessionUID: "test session id")
+        let user: UserManager = UserManager(api: apiService, userinfo: testUserInfo, auth: testAuth, parent: users)
+        users.users.append(user)
+        return users.firstUser?.userInfo.userId
+    }
+
+    private func createTestSearchIndexDB() {
+        self.testSearchIndexDBName = EncryptedSearchIndexService.shared.getSearchIndexName(self.testUserID)
         self.connectionToSearchIndexDB = EncryptedSearchIndexService.shared.connectToSearchIndex(for: self.testUserID)!
         EncryptedSearchIndexService.shared.createSearchIndexTable(using: self.connectionToSearchIndexDB)
 
@@ -63,7 +94,24 @@ class EncryptedSearchTests: XCTestCase {
         EncryptedSearchService.shared.addMessageKewordsToSearchIndex(testUserID, testMessageSecond, encryptedContent2, false)
     }
 
-    func deleteTestSearchIndexDB() throws {
+    private func setupCoreData() throws {
+        coreDataService = CoreDataService(container: CoreDataStore.shared.memoryPersistentContainer)
+        testContext = coreDataService.rootSavingContext
+
+        let parsedLabel = testLabelsData.parseJson()!
+        _ = try GRTJSONSerialization.objects(withEntityName: Label.Attributes.entityName,
+                                             fromJSONArray: parsedLabel,
+                                             in: testContext)
+
+        try testContext.save()
+    }
+
+    private func deleteCoreData() {
+        coreDataService = nil
+        testContext = nil
+    }
+
+    private func deleteTestSearchIndexDB() throws {
         // Create the path to the database for user 'test'.
         let pathToTestDB: String = EncryptedSearchIndexService.shared.getSearchIndexPathToDB(self.testSearchIndexDBName)
         let urlToDB: URL? = URL(string: pathToTestDB)
@@ -74,6 +122,18 @@ class EncryptedSearchTests: XCTestCase {
 
         // Remove the database file.
         try FileManager.default.removeItem(atPath: urlToDB!.path)
+    }
+
+    private func makeTestMessageIn(_ labelId: String) -> Message? {
+        let parsedObject = testMessageMetaData.parseObjectAny()!
+        let message = try? GRTJSONSerialization
+            .object(withEntityName: Message.Attributes.entityName,
+                    fromJSONDictionary: parsedObject,
+                    in: testContext) as? Message
+        message?.remove(labelID: "0")
+        message?.add(labelID: labelId)
+        try? testContext.save()
+        return message
     }
 
     func testEncryptedSearchServiceSingleton() throws {
@@ -186,7 +246,14 @@ class EncryptedSearchTests: XCTestCase {
     }
 
     func testInsertSingleMessageToSearchIndex() throws {
-        //TODO
+        let sut = EncryptedSearchService.shared.insertSingleMessageToSearchIndex
+        let message = try XCTUnwrap(makeTestMessageIn(Message.Location.allmail.rawValue))
+        sut(message)
+        // Wait for the message to be inserted
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
+            let numberOfMessagesInSearchIndex: Int = EncryptedSearchIndexService.shared.getNumberOfEntriesInSearchIndex(for: self.testUserID)
+            XCTAssertEqual(numberOfMessagesInSearchIndex, 3)
+        }
     }
 
     func testDeleteMessageFromSearchIndex() throws {
