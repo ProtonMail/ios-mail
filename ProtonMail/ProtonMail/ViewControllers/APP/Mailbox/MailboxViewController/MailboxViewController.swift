c2ff86a2ad7810df81f47d5da014b7ded7d51612
Ralph Ankele <ralph.ankele@proton.ch>
2021-12-14 15:35:46 +0100

    react to internal update to the messages and mirrow to existing search index and cache

--- ProtonMail/ProtonMail/ViewControllers/APP/Mailbox/MailboxViewController/MailboxViewController.swift
+++ ProtonMail/ProtonMail/ViewControllers/APP/Mailbox/MailboxViewController/MailboxViewController.swift
@@ -2277,7 +2277,7 @@ extension MailboxViewController: NSFetchedResultsControllerDelegate {
 
         // If encrypted search is turned on update the search index
         if userCachedStatus.isEncryptedSearchOn {
-            EncryptedSearchService.shared.updateSearchIndex(type, anObject as? Message, indexPath, newIndexPath)
+            EncryptedSearchService.shared.updateSearchIndex(type, anObject as? Message)
         }
     }
 }
