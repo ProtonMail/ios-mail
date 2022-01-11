4d7307e5a97534f96833dac67807109e8bdecafd
Ralph Ankele <ralph.ankele@proton.ch>
2022-01-11 10:41:30 +0100

    fix rebase conflict - add EncryptedSearchIndexService

---
+++ ProtonMail/ProtonMailCommon/DataServices/EncryptedSearchIndexService.swift
@@ -0,0 +1,23 @@
+//
+//  EncryptedSearchIndexService.swift
+//  ProtonMail
+//
+//  Created by Ralph Ankele on 19.07.21.
+//  Copyright © 2021 ProtonMail. All rights reserved.
+//
+
+import Foundation
+
+public class EncryptedSearchIndexService {
+    //instance of Singleton
+    static let shared = EncryptedSearchIndexService()
+    
+    //set initializer to private - Singleton
+    private init(){
+        //TODO
+    }
+}
+
+extension EncryptedSearchIndexService {
+    //TODO add functions to build the search index
+}
