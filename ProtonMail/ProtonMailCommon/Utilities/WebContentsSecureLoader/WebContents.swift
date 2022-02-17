6466e1941e63e931c4464584552b006d48c53908
Ralph Ankele <ralph.ankele@proton.ch>
2022-02-17 10:46:04 +0100

    add highlighting of search keywords in body of email

--- ProtonMail/ProtonMailCommon/Utilities/WebContentsSecureLoader/WebContents.swift
+++ ProtonMail/ProtonMailCommon/Utilities/WebContentsSecureLoader/WebContents.swift
@@ -21,6 +21,7 @@
 //  along with ProtonMail.  If not, see <https://www.gnu.org/licenses/>.
 
 import WebKit
+import ProtonCore_DataModel
 
 /// Contains HTML to be loaded into WebView and appropriate CSP
 class WebContents {
@@ -37,8 +38,22 @@ class WebContents {
          remoteContentMode: RemoteContentPolicy,
          renderStyle: MessageRenderStyle = .dark,
          supplementCSS: String? = nil) {
-        // \u00A0 is white space that will break dompurify
-        self.body = body.preg_replace("\u{00A0}", replaceto: " ")
+        if UserInfo.isEncryptedSearchEnabled {
+            var highlightedBody: String = body
+            let usersManager: UsersManager = sharedServices.get(by: UsersManager.self)
+            if let userID = usersManager.firstUser?.userInfo.userId {
+                let expectedESStates: [EncryptedSearchService.EncryptedSearchIndexState] = [.partial, .complete]
+                if expectedESStates.contains(EncryptedSearchService.shared.getESState(userID: userID)) {
+                    highlightedBody = EncryptedSearchService.shared.highlightKeyWords(bodyAsHtml: body)
+                }
+            }
+
+            // \u00A0 is white space that will break dompurify
+            self.body = highlightedBody.preg_replace("\u{00A0}", replaceto: " ")
+        } else {
+            // \u00A0 is white space that will break dompurify
+            self.body = body.preg_replace("\u{00A0}", replaceto: " ")
+        }
         self.remoteContentMode = remoteContentMode
         self.renderStyle = renderStyle
         self.supplementCSS = supplementCSS
