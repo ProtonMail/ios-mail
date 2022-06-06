1dc1c28721e3e3d9d68f617bc1f8b2dce23cfdf6
AnsonChen <jianyou.chen@proton.ch>
2022-06-06 17:56:51 +0800

    MAILIOS-2561, Improvement composer body voice over

--- ProtonMail/ProtonMail/Controller/MessageDetail/Modern/EmbeddableCompose/ContainableComposeViewController.swift
+++ ProtonMail/ProtonMail/Controller/MessageDetail/Modern/EmbeddableCompose/ContainableComposeViewController.swift
@@ -38,9 +38,6 @@ class ContainableComposeViewController: ComposeViewController, BannerRequester {
         super.viewDidLoad()
         view.backgroundColor = ColorProvider.BackgroundNorm
         self.webView.scrollView.clipsToBounds = false
-        self.webView.isAccessibilityElement = true
-        self.webView.accessibilityIdentifier = "ComposerBody"
-        self.webView.accessibilityLabel = LocalString._composer_voiceover_message_content
         
         self.heightObservation = self.htmlEditor.observe(\.contentHeight, options: [.new, .old]) { [weak self] htmlEditor, change in
             guard let self = self, change.oldValue != change.newValue else { return }
