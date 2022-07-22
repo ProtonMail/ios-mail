888e5fe959e0791170eed970f651e10e1183f2a3
Ralph Ankele <ralph.ankele@proton.ch>
2022-07-22 13:16:04 +0200

    fix LIBES-237: indexing stuck after quiting and reopening app

--- ProtonMail/ProtonMail/Services/APP_share/EncryptedSearchService/IndexSingleMessageAsyncOperation.swift
+++ ProtonMail/ProtonMail/Services/APP_share/EncryptedSearchService/IndexSingleMessageAsyncOperation.swift
@@ -86,6 +86,10 @@ open class IndexSingleMessageAsyncOperation: Operation {
                                                                                  userID: self.userID,
                                                                                  isUpdate: false) { [weak self] in
                     userCachedStatus.encryptedSearchProcessedMessages += 1
+                    userCachedStatus.encryptedSearchLastMessageTimeIndexed =
+                    EncryptedSearchIndexService.shared.getOldestMessageInSearchIndex(for: self?.userID ?? "").asInt
+                    userCachedStatus.encryptedSearchLastMessageIDIndexed =
+                    EncryptedSearchIndexService.shared.getMessageIDOfOldestMessageInSearchIndex(for: self?.userID ?? "")
                     EncryptedSearchService.shared.updateProgressedMessagesUI()
                     self?.state = .finished
                 }
@@ -94,6 +98,10 @@ open class IndexSingleMessageAsyncOperation: Operation {
                                                                                  userID: self.userID,
                                                                                  isUpdate: false) { [weak self] in
                     userCachedStatus.encryptedSearchProcessedMessages += 1
+                    userCachedStatus.encryptedSearchLastMessageTimeIndexed =
+                    EncryptedSearchIndexService.shared.getOldestMessageInSearchIndex(for: self?.userID ?? "").asInt
+                    userCachedStatus.encryptedSearchLastMessageIDIndexed =
+                    EncryptedSearchIndexService.shared.getMessageIDOfOldestMessageInSearchIndex(for: self?.userID ?? "")
                     EncryptedSearchService.shared.updateProgressedMessagesUI()
                     self?.state = .finished
                 }
