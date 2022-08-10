9eedcfcb5c3f4fed583ecc40736c2444d917bbc5
Ralph Ankele <ralph.ankele@proton.ch>
2022-08-10 13:33:03 +0200

    delete message from encrypted search index if permanently delete

--- ProtonMail/ProtonMail/Services/APP_share/MessageDataService/MessageDataService+MessageActions.swift
+++ ProtonMail/ProtonMail/Services/APP_share/MessageDataService/MessageDataService+MessageActions.swift
@@ -106,6 +106,12 @@ extension MessageDataService {
             .map(\.messageID.rawValue)
             .filter { UUID(uuidString: $0) == nil }
         self.queue(.delete(currentLabelID: nil, itemIDs: messagesIds))
+
+        //Delete from encrypted search index
+        for message in messages {
+            EncryptedSearchService.shared.deleteMessageFromSearchIndex(message)
+        }
+
         return true
     }
 
