f34dc9b476e9bc92edbf99162e2ab802c90a2686
Ralph Ankele <ralph.ankele@proton.ch>
2022-08-30 17:45:06 +0200

    Fix LIBES-174: keyword highlighting

--- ProtonMail/ProtonMail/ViewControllers/APP/Mailbox/Cells/NewMailboxMessageView/NewMailboxMessageCellPresenter.swift
+++ ProtonMail/ProtonMail/ViewControllers/APP/Mailbox/Cells/NewMailboxMessageView/NewMailboxMessageCellPresenter.swift
@@ -20,8 +20,8 @@
 //  You should have received a copy of the GNU General Public License
 //  along with Proton Mail.  If not, see <https://www.gnu.org/licenses/>.
 
-import ProtonCore_UIFoundations
 import ProtonCore_DataModel
+import ProtonCore_UIFoundations
 import UIKit
 
 class NewMailboxMessageCellPresenter {
