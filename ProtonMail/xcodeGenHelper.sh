2d31ea45a440a74f2f746eb02b29faf37fb4aff3
Jacek Krasiukianis <jacek.krasiukianis@proton.ch>
2022-09-16 11:12:27 +0000

    Move all project tools to Mint

--- ProtonMail/xcodeGenHelper.sh
+++ ProtonMail/xcodeGenHelper.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-# How to use 
+# How to use
 # sh xcodeGenHelper.sh
 
-# If pod repo needs to be updated   
-# add flag --repo-update 
+# If pod repo needs to be updated
+# add flag --repo-update
 # sh xcodeGenHelper.sh --repo-update
 
 protonDirectory=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 cd $protonDirectory
 
-xcodeGen/xcodegen_2.32.0 --spec project.json --project ./ --use-cache
+mint run xcodegen --spec project.json --project ./ --use-cache
 if [ "$1" == "--repo-update" ]; then
     yes | pod install --repo-update
-else 
+else
     yes | pod install
 fi
