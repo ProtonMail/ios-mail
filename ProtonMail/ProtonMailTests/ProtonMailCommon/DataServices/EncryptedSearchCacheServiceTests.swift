f662166a34102ce690922ce72e96710a9b313cb5
Ralph Ankele <ralph.ankele@proton.ch>
2022-09-19 15:55:12 +0200

    add new unit tests for EncryptedSearchCacheService

---
+++ ProtonMail/ProtonMailTests/ProtonMailCommon/DataServices/EncryptedSearchCacheServiceTests.swift
@@ -0,0 +1,54 @@
+// Copyright (c) 2021 Proton Technologies AG
+//
+// This file is part of ProtonMail.
+//
+// ProtonMail is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// ProtonMail is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with ProtonMail. If not, see https://www.gnu.org/licenses/.
+
+import XCTest
+@testable import ProtonMail
+
+class EncryptedSearchCacheServiceTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+
+        //TODO
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+
+        //TODO
+    }
+
+    func testBuildCacheForUser() throws {
+        //TODO
+    }
+
+    func testDeleteCache() throws {
+        //TODO
+    }
+
+    func testUpdateCachedMessage() throws {
+        //TODO
+    }
+
+    func testDeleteCachedMessage() throws {
+        //TODO
+    }
+
+    func testIsCacheBuilt() throws {
+        //TODO
+    }
+}
