9878c62865eb650b90533cb0887d68a3e6a8ec99
Ralph Ankele <ralph.ankele@proton.ch>
2022-09-19 15:03:05 +0200

    Rebranding with core 3.15.0 V5

---
+++ ProtonMail/ProtonMail/Controller/Coordinator/Deeplinkable.swift
@@ -0,0 +1,27 @@
+//
+//  Deeplinkable.swift
+//  ProtonMail - Created on 23/07/2019.
+//
+//
+//  Copyright (c) 2019 Proton AG
+//
+//  This file is part of Proton Mail.
+//
+//  Proton Mail is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  Proton Mail is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with Proton Mail.  If not, see <https://www.gnu.org/licenses/>.
+
+import UIKit
+
+protocol Deeplinkable: AnyObject {
+    var deeplinkNode: DeepLink.Node { get }
+}
