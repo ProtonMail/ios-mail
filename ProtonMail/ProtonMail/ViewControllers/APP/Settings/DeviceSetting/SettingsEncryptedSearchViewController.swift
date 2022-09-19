28ea0862a055cf30a9f7ff84c049e5dac13613c9
Ralph Ankele <ralph.ankele@proton.ch>
2022-09-19 15:04:17 +0200

    add settings encryptedsearchviewcontroller

---
+++ ProtonMail/ProtonMail/ViewControllers/APP/Settings/DeviceSetting/SettingsEncryptedSearchViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SettingsEncryptedSearchViewController.swift
+//  ProtonMail
+//
+//  Created by Ralph Ankele on 30.06.21.
+//  Copyright © 2021 ProtonMail. All rights reserved.
+//
+
+import UIKit
+
+class SettingsEncryptedSearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
