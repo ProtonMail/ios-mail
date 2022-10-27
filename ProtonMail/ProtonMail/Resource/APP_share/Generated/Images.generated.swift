0cb19397b63ca0e62927138f7c8d7935cb6753eb
Jacek Krasiukianis <jacek.krasiukianis@proton.ch>
2022-10-27 14:20:02 +0200

    Adjust stencil, re-run SwiftGen after last cleanup

--- ProtonMail/ProtonMail/Resource/APP_share/Generated/Images.generated.swift
+++ ProtonMail/ProtonMail/Resource/APP_share/Generated/Images.generated.swift
@@ -9,10 +9,6 @@
   import UIKit
 #endif
 
-// Deprecated typealiases
-@available(*, deprecated, renamed: "ImageAsset.Image", message: "This typealias will be removed in SwiftGen 7.0")
-internal typealias AssetImageTypeAlias = ImageAsset.Image
-
 // swiftlint:disable superfluous_disable_command file_length implicit_return
 
 // MARK: - Asset Catalogs
@@ -20,9 +16,10 @@ internal typealias AssetImageTypeAlias = ImageAsset.Image
 // swiftlint:disable identifier_name line_length nesting type_body_length type_name
 internal enum Asset {
   internal static let composeScheduleStar = ImageAsset(name: "compose_schedule_star")
-  internal static let contactGroupsCheck = ImageAsset(name: "contact_groups_check")
-  internal static let contactGroupsIcon = ImageAsset(name: "contact_groups_icon")
   internal static let esIcon = ImageAsset(name: "es-icon")
+  internal static let launchScreenBackground = ColorAsset(name: "LaunchScreenBackground")
+  internal static let launchScreenBrand = ImageAsset(name: "launchScreenBrand")
+  internal static let launchScreenLogo = ImageAsset(name: "launchScreenLogo")
   internal static let conversationNotice = ImageAsset(name: "conversationNotice")
   internal static let mailAttachmentDoc = ImageAsset(name: "mail_attachment-doc")
   internal static let mailAttachmentFile = ImageAsset(name: "mail_attachment-file")
@@ -58,6 +55,8 @@ internal enum Asset {
   internal static let icMagnifier = ImageAsset(name: "ic-magnifier")
   internal static let icPenSquare = ImageAsset(name: "ic-pen-square")
   internal static let icStarFilled = ImageAsset(name: "ic-star-filled")
+  internal static let icChevronDown = ImageAsset(name: "ic-chevron-down")
+  internal static let icChevronUp = ImageAsset(name: "ic-chevron-up")
   internal static let pinCodeDel = ImageAsset(name: "pin_code_del")
   internal static let popupBehindImage = ImageAsset(name: "popup_behind_image")
   internal static let touchIdIcon = ImageAsset(name: "touch_id_icon")
@@ -68,6 +67,54 @@ internal enum Asset {
 // swiftlint:enable identifier_name line_length nesting type_body_length type_name
 
 // MARK: - Implementation Details
+
+internal final class ColorAsset {
+  internal fileprivate(set) var name: String
+
+  #if os(macOS)
+  internal typealias Color = NSColor
+  #elseif os(iOS) || os(tvOS) || os(watchOS)
+  internal typealias Color = UIColor
+  #endif
+
+  @available(iOS 11.0, tvOS 11.0, watchOS 4.0, macOS 10.13, *)
+  internal private(set) lazy var color: Color = {
+    guard let color = Color(asset: self) else {
+      fatalError("Unable to load color asset named \(name).")
+    }
+    return color
+  }()
+
+  #if os(iOS) || os(tvOS)
+  @available(iOS 11.0, tvOS 11.0, *)
+  internal func color(compatibleWith traitCollection: UITraitCollection) -> Color {
+    let bundle = BundleToken.bundle
+    guard let color = Color(named: name, in: bundle, compatibleWith: traitCollection) else {
+      fatalError("Unable to load color asset named \(name).")
+    }
+    return color
+  }
+  #endif
+
+  fileprivate init(name: String) {
+    self.name = name
+  }
+}
+
+internal extension ColorAsset.Color {
+  @available(iOS 11.0, tvOS 11.0, watchOS 4.0, macOS 10.13, *)
+  convenience init?(asset: ColorAsset) {
+    let bundle = BundleToken.bundle
+    #if os(iOS) || os(tvOS)
+    self.init(named: asset.name, in: bundle, compatibleWith: nil)
+    #elseif os(macOS)
+    self.init(named: NSColor.Name(asset.name), bundle: bundle)
+    #elseif os(watchOS)
+    self.init(named: asset.name)
+    #endif
+  }
+}
+
 
 internal struct ImageAsset {
   internal fileprivate(set) var name: String
@@ -136,4 +183,3 @@ private final class BundleToken {
   }()
 }
 // swiftlint:enable convenience_type
-
