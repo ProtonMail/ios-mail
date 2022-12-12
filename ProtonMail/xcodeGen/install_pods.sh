451dc2c63f7a6a572de75b7cc37ca13c8a2fc365
Jacek Krasiukianis <jacek.krasiukianis@proton.ch>
2022-12-12 18:34:10 +0100

    MAILIOS-3056 Core 3.24.5

--- ProtonMail/xcodeGen/install_pods.sh
+++ ProtonMail/xcodeGen/install_pods.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
 
-# yes is needed to go through SSH key fingerprint prompts on CI
-if ! yes | pod install; then
-    echo "pod install failed. It might be because the local repo is outdated - will refresh and retry once."
-    pod cache clean --all --verbose
-    rm -rfv ~/.cocoapods/repos/
-    pod install --repo-update --verbose
-fi
+# Do not need the access to pod repo while generating the project file.
+pod install --no-repo-update
