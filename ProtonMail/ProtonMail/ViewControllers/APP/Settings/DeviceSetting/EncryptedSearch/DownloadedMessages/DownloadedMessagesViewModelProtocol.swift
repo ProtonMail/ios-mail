90474bdc3e31b89c7eec25fede60e85ff880b75a
Xavi Gil <xavi.gil@proton.ch>
2023-02-17 11:26:59 +0000

    MAILIOS-3151: Encrypted Search Settings 4/x

---
+++ ProtonMail/ProtonMail/ViewControllers/APP/Settings/DeviceSetting/EncryptedSearch/DownloadedMessages/DownloadedMessagesViewModelProtocol.swift
@@ -0,0 +1,47 @@
+// Copyright (c) 2023 Proton Technologies AG
+//
+// This file is part of Proton Mail.
+//
+// Proton Mail is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Proton Mail is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Proton Mail. If not, see https://www.gnu.org/licenses/.
+
+import Foundation
+
+protocol DownloadedMessagesViewModelProtocol {
+    var input: DownloadedMessagesViewModelInput { get }
+    var output: DownloadedMessagesViewModelOutput { get }
+}
+
+protocol DownloadedMessagesViewModelInput {
+    func viewWillAppear()
+    func didChangeStorageLimitValue(newValue: ByteCount)
+    func didTapClearStorageUsed()
+}
+
+protocol DownloadedMessagesViewModelOutput {
+    var sections: [DownloadedMessagesSection] { get }
+    var storageLimitSelected: ByteCount { get }
+    var localStorageUsed: ByteCount { get }
+
+    func setUIDelegate(_ delegate: DownloadedMessagesUIProtocol)
+}
+
+protocol DownloadedMessagesUIProtocol: AnyObject {
+    func reloadData()
+}
+
+enum DownloadedMessagesSection {
+    case messageHistory
+    case storageLimit
+    case localStorageUsed
+}
