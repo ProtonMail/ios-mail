f257c99a2329da008688faef9953068ca081f75a
Xavi Gil <xavi.gil@proton.ch>
2023-03-10 10:45:41 +0100

    Revert "Suggested code review"
    This reverts commit 1c519b3bb8fc5bb77e9037a2fe7e4b59a9dbd64f.

--- ProtonMail/xcodeGen/copySettingsEnvironmentBundle.sh
+++ ProtonMail/xcodeGen/copySettingsEnvironmentBundle.sh
@@ -1,11 +1,10 @@
 # For DEBUG and ENTERPRISE configurations, we replace the settings bundle with one to be able to switch environments
-if ! [[ "${CONFIGURATION}" = "Release" ]]; then
-    groupContainerIdentifier="group.ch.protonmail.protonmail"
-    if [[ "${CONFIGURATION}" = "Enterprise Debug" || "${CONFIGURATION}" = "Enterprise Release" ]]; then
-        groupContainerIdentifier="group.com.protonmail.protonmail"
-    fi
-
+if [[ "${CONFIGURATION}" = "Debug" ]]; then
     rm -r "${BUILT_PRODUCTS_DIR}/${PRODUCT_NAME}.app/Settings.bundle"
-    /usr/libexec/PlistBuddy -c "Set :ApplicationGroupContainerIdentifier $groupContainerIdentifier" ${PROJECT_DIR}/ProtonMail/Supporting\ Files/debug/SettingsEnvironment.bundle/Root.plist
     cp -r ${PROJECT_DIR}/ProtonMail/Supporting\ Files/debug/SettingsEnvironment.bundle "${BUILT_PRODUCTS_DIR}/${PRODUCT_NAME}.app/Settings.bundle"
+fi
+
+if [[ "${CONFIGURATION}" = "Enterprise Debug" || "${CONFIGURATION}" = "Enterprise Release" ]]; then
+    rm -r "${BUILT_PRODUCTS_DIR}/${PRODUCT_NAME}.app/Settings.bundle"
+    cp -r ${PROJECT_DIR}/ProtonMail/Supporting\ Files/enterprise/SettingsEnvironment.bundle "${BUILT_PRODUCTS_DIR}/${PRODUCT_NAME}.app/Settings.bundle"
 fi
