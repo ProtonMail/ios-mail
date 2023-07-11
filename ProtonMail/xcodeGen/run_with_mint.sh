fec54afd4cb7a535dd1b0e983c5fdcce9f9e4617
Jacek Krasiukianis <jacek.krasiukianis@proton.ch>
2023-07-11 09:54:02 +0200

    Fix xcrun error

--- ProtonMail/xcodeGen/run_with_mint.sh
+++ ProtonMail/xcodeGen/run_with_mint.sh
@@ -2,13 +2,32 @@
 
 set -eo pipefail
 
-REPOSITORY_ROOT=$(git rev-parse --show-toplevel)
-PATH_TO_MINTFILE="$REPOSITORY_ROOT/Mintfile"
-MINT_CMD="xcrun --sdk macosx mint"
-
-if ! $MINT_CMD which "$1" --mintfile $PATH_TO_MINTFILE > /dev/null; then
-  echo "error: Expected version of $1 not found. Install it by executing \"mint bootstrap\" in project root directory."
-  exit 65
+# This is to tell Xcode where to find Homebrew and therefore Mint.
+# This is a very poor solution that
+# 1. assumes Mint has been installed via Homebrew
+# 2. assumes Homebrew has been installed to the default location
+#
+# Ideally we would just tell Xcode where to find the Mint executable.
+# Note: Xcode has its own, sanitised PATH; the PATH you see below is not your user's PATH - that one most likely already includes the Homebrew directory.
+if test -d "/opt/homebrew/bin/"; then
+  PATH="/opt/homebrew/bin/:${PATH}"
 fi
 
-$MINT_CMD run --mintfile $PATH_TO_MINTFILE "$@"
+if test -d "/Users/proton/brew/bin/"; then
+  PATH="/Users/proton/brew/bin/:${PATH}"
+fi
+
+if ! command -v mint >/dev/null; then
+  echo "error: mint not found. Install it by executing \"brew install mint\" or if you already have it installed somewhere, ask the Mail team to update the script."
+  exit 127
+fi
+
+REPOSITORY_ROOT=$(git rev-parse --show-toplevel)
+PATH_TO_MINTFILE="$REPOSITORY_ROOT/Mintfile"
+
+if ! mint which "$1" --mintfile $PATH_TO_MINTFILE > /dev/null; then
+  echo "error: Expected version of $1 not found. Install it by executing \"mint bootstrap\" in project root directory."
+  exit 127
+fi
+
+mint run --mintfile $PATH_TO_MINTFILE "$@"
