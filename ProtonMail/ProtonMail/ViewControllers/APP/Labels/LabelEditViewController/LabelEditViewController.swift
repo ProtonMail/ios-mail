b5e0e91892519212b0b054a58a7534fb18f41892
AnsonChen <jianyou.chen@proton.ch>
2023-08-11 13:55:01 +0800

    MAILIOS-3567, Inconsistency in the spelling of colo(u)r

--- ProtonMail/ProtonMail/ViewControllers/APP/Labels/LabelEditViewController/LabelEditViewController.swift
+++ ProtonMail/ProtonMail/ViewControllers/APP/Labels/LabelEditViewController/LabelEditViewController.swift
@@ -221,7 +221,7 @@ extension LabelEditViewController {
     override func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
         switch viewModel.output.sections[section] {
         case .palette, .colorInherited:
-            return PMHeaderView(title: LocalString._select_colour,
+            return PMHeaderView(title: LocalString._select_color,
                                 font: UIFont.preferredFont(for: .subheadline, weight: .regular))
         default:
             return PMHeaderView(title: "")
