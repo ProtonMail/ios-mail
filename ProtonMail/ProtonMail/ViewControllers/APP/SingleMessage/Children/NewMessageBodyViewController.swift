42a4e5cc42957662778d00c6fa8c3164d6b2fcbe
AnsonChen <jianyou.chen@proton.ch>
2024-01-11 21:13:33 +0800

    Try to fix extra space in message detail

--- ProtonMail/ProtonMail/ViewControllers/APP/SingleMessage/Children/NewMessageBodyViewController.swift
+++ ProtonMail/ProtonMail/ViewControllers/APP/SingleMessage/Children/NewMessageBodyViewController.swift
@@ -335,7 +335,16 @@ class NewMessageBodyViewController: UIViewController {
 
     @objc
     private func doEnterForeground() {
-        let script = "document.body.scrollHeight"
+        let script = """
+        const bodyHeight = document.body.scrollHeight;
+        const radio = document
+        .querySelector('meta[name="viewport"]')
+        .attributes['content']
+        .value
+        .match(/initial-scale=(.*?),/)[1];
+        
+        bodyHeight * radio
+        """
         webView?.evaluateJavaScript(script, completionHandler: { [weak self] result, error in
             // JavaScript seems like won't execute in the background
             // If message is opened in the background, webView height could not be updated to the correct value
