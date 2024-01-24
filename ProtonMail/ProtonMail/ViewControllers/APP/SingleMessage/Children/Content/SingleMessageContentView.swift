277875c75ef13ce964feac88ac1d8deba2a33075
Jacek Krasiukianis <jacek.krasiukianis@proton.ch>
2024-01-24 11:00:33 +0000

    MAILIOS-4109 Fix missing line under message header

--- ProtonMail/ProtonMail/ViewControllers/APP/SingleMessage/Children/Content/SingleMessageContentView.swift
+++ ProtonMail/ProtonMail/ViewControllers/APP/SingleMessage/Children/Content/SingleMessageContentView.swift
@@ -11,6 +11,7 @@ class SingleMessageContentView: UIView {
     lazy var messageHeaderContainer = HeaderContainerView(replyState: replyState)
     let attachmentContainer = UIView()
     let stackView = UIStackView.stackView(axis: .vertical)
+    let separator = SubviewsFactory.smallSeparatorView
     let footerButtons = SingleMessageFooterButtons()
 
     init(replyState: HeaderContainerView.ReplyState) {
@@ -42,6 +43,7 @@ class SingleMessageContentView: UIView {
 
         footerButtons.setContentHuggingPriority(.defaultLow, for: .horizontal)
         footerButtons.setContentHuggingPriority(.required, for: .vertical)
+        messageHeaderContainer.addSubview(separator)
     }
 
     private func setUpLayout() {
@@ -55,6 +57,13 @@ class SingleMessageContentView: UIView {
         [
             attachmentContainer.heightAnchor.constraint(equalToConstant: 0).setPriority(as: .defaultLow),
             bannerContainer.heightAnchor.constraint(equalToConstant: 0).setPriority(as: .defaultLow)
+        ].activate() 
+
+        [
+            separator.leadingAnchor.constraint(equalTo: messageHeaderContainer.leadingAnchor),
+            separator.trailingAnchor.constraint(equalTo: messageHeaderContainer.trailingAnchor),
+            separator.bottomAnchor.constraint(equalTo: messageHeaderContainer.bottomAnchor),
+            separator.heightAnchor.constraint(equalToConstant: 1)
         ].activate()
     }
 
@@ -68,4 +77,12 @@ class SingleMessageContentView: UIView {
         nil
     }
 
+}
+
+private enum SubviewsFactory {
+    static var smallSeparatorView: UIView {
+        let view = UIView(frame: .zero)
+        view.backgroundColor = ColorProvider.Shade20
+        return view
+    }
 }
