d1e9c6b3449d0a91dc90f703ff2eae992d9618a6
Xavi Gil <xavi.gil@proton.ch>
2024-02-26 11:49:55 +0100

    initial commit

---
+++ ios/ProtonMailApp.swift
@@ -0,0 +1,17 @@
+//
+//  iosApp.swift
+//  ios
+//
+//  Created by xavi on 26/2/24.
+//
+
+import SwiftUI
+
+@main
+struct ProtonMail: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
