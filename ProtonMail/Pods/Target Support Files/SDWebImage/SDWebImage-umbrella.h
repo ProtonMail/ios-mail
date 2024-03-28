1da3150227e820404ae54b55a3138ba97d0e8f09
Dara Maksimoska <dara.maksimoska@proton.ch>
2024-03-28 08:38:06 +0000

    MAILIOS-3931 "Test/ trash and permanently delete one message"

--- ProtonMail/Pods/Target Support Files/SDWebImage/SDWebImage-umbrella.h
+++ ProtonMail/Pods/Target Support Files/SDWebImage/SDWebImage-umbrella.h
@@ -69,6 +69,7 @@
 #import "UIImageView+WebCache.h"
 #import "UIView+WebCache.h"
 #import "UIView+WebCacheOperation.h"
+#import "UIView+WebCacheState.h"
 #import "SDWebImage.h"
 
 FOUNDATION_EXPORT double SDWebImageVersionNumber;
