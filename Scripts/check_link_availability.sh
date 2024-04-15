d02bc541e7cbca1a19c2e0b9b739ddbc0be2db91
Jacek Krasiukianis <jacek.krasiukianis@proton.ch>
2024-04-15 16:04:56 +0200

    Remove linting stage

--- Scripts/check_link_availability.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -euo pipefail
-
-for link in $(grep -r "= \"https://" --include "*.swift" --exclude-dir={Pods,ProtonMailUITests,ProtonMailTests} ProtonMail | cut -d"\"" -f2); do
-    echo "Checking $link..."
-    curl --fail --output /dev/null --show-error --silent $link
-done
