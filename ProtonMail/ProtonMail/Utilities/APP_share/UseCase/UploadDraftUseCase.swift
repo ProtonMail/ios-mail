c4b9321f8d197fdd47e521e1b4109705fd11407d
Jacek Krasiukianis <jacek.krasiukianis@proton.ch>
2024-07-12 14:35:53 +0200

    INBIOS-50 Prevent empty warning from showing up, add Sentry logs

--- ProtonMail/ProtonMail/Utilities/APP_share/UseCase/UploadDraftUseCase.swift
+++ ProtonMail/ProtonMail/Utilities/APP_share/UseCase/UploadDraftUseCase.swift
@@ -44,8 +44,12 @@ final class UploadDraft: UploadDraftUseCase {
             if let responseError = error as? ResponseError,
                let underlyingError = responseError.underlyingError {
                 if underlyingError.code != APIErrorCode.updateDraftHasBeenSent {
-                    SystemLogger.log(error: error, category: .emptyAlert)
-                    // Doesn't need to show message has sent message to user
+                    if underlyingError.localizedDescription.isEmpty {
+                        let code = underlyingError.bestShotAtReasonableErrorCode
+                        PMAssertionFailure("Attempting to display error with empty description, code \(code)")
+                    } else {
+                        SystemLogger.log(error: error, category: .emptyAlert)
+                    }
                     await NSError.alertSavingDraftError(details: underlyingError.localizedDescription)
                 }
 
@@ -55,7 +59,12 @@ final class UploadDraft: UploadDraftUseCase {
             } else if error is UploadDraftError {
                 SystemLogger.log(message: "UploadDraftError: \(error)", isError: true)
             } else {
-                SystemLogger.log(error: error, category: .emptyAlert)
+                if error.localizedDescription.isEmpty {
+                    let code = error.bestShotAtReasonableErrorCode
+                    PMAssertionFailure("Attempting to display error with empty description, code \(code)")
+                }else {
+                    SystemLogger.log(error: error, category: .emptyAlert)
+                }
                 await NSError.alertSavingDraftError(details: error.localizedDescription)
             }
             throw error
