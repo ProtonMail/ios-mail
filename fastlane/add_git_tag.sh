f2af15737be3fcf0895d80e67e00e34cda25550b
Maciej Gomółka <maciej.gomolka@proton.ch>
2024-07-22 14:19:13 +0200

    Update add_git_tag.sh

--- fastlane/add_git_tag.sh
+++ fastlane/add_git_tag.sh
@@ -6,6 +6,6 @@ BUILD_NUMBER=$(echo "$BUILD_SETTINGS" | grep CURRENT_PROJECT_VERSION | awk '{pri
 GIT_TAG_NAME="v${VERSION_NUMBER}_${BUILD_NUMBER}"
 echo $GIT_TAG_NAME
 
-git remote set-url origin "https://Tags_publisher:${TAGS_PUBLISHER_TOKEN}@$(awk -F '@' '{print $2}' <<< "$CI_REPOSITORY_URL")";
+git remote set-url origin "https://${GIT_CI_USERNAME}:${TAGS_PUBLISHER_TOKEN}@$(awk -F '@' '{print $2}' <<< "$CI_REPOSITORY_URL")";
 git tag $GIT_TAG_NAME
 git push origin $GIT_TAG_NAME
