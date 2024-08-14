0fa9d0c3385187af1c757bd1ca6581c0625f6331
Mateusz Szklarek <mateusz.szklarek@proton.ch>
2024-08-14 16:09:50 +0200

    Fix missing native swipe-back gesture - from left edge to right
    Hiding the native back bar button item in SwiftUI disables native
    swipe-back gesture which is a default gesture in iOS
    (https://stackoverflow.com/a/58045361).
    
    To keep the gesture it's necessary to extend `UINavigationController` with
    `UIGestureRecognizerDelegate` protocol and configure
    interactivePopGestureRecognizer's delegate which is used under
    the hood by the SwiftUI for navigation. The other option would be to
    use `UINavigationController` wrapped up with `UIHostingController` but
    this requires more work.

---
+++ ios/UI/Screens/Navigation/UINavigationController+UIGestureRecognizerDelegate.swift
@@ -0,0 +1,41 @@
+// Copyright (c) 2024 Proton Technologies AG
+//
+// This file is part of Proton Mail.
+//
+// Proton Mail is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Proton Mail is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Proton Mail. If not, see https://www.gnu.org/licenses/.
+
+import UIKit
+
+/**
+ Hiding native back bar button item in SwiftUI disables native swipe-back
+ gesture which is a default gesture in iOS.
+
+ To keep the gesture it's necessary to extend `UINavigationController`
+ and set interactivePopGestureRecognizer's delegate which is used under
+ the hood by the SwiftUI for navigation.
+ */
+extension UINavigationController: UIGestureRecognizerDelegate {
+
+    override open func viewDidLoad() {
+        super.viewDidLoad()
+        interactivePopGestureRecognizer?.delegate = self
+    }
+
+    // MARK: - UIGestureRecognizerDelegate
+
+    public func gestureRecognizerShouldBegin(_ gestureRecognizer: UIGestureRecognizer) -> Bool {
+        return viewControllers.count > 1
+    }
+
+}
