09d6dbf5989b1e902bd4bafcb1af087abcaee92a
Mateusz Szklarek <mateusz.szklarek@proton.ch>
2024-10-10 13:01:44 +0200

    Rename `Contacts` to `ProtonContacts` because of the conflict with system framework

---
+++ Modules/ProtonContacts/Package.swift
@@ -0,0 +1,28 @@
+// swift-tools-version: 5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "ProtonContacts",
+    platforms: [.iOS(.v17)],
+    products: [
+        .library(
+            name: "ProtonContacts",
+            targets: ["ProtonContacts"]
+        ),
+    ],
+    dependencies: [
+        .package(path: "DesignSystem")
+    ],
+    targets: [
+        .target(
+            name: "ProtonContacts",
+            dependencies: ["DesignSystem"]
+        ),
+        .testTarget(
+            name: "ProtonContactsTests",
+            dependencies: [.target(name: "ProtonContacts")]
+        ),
+    ]
+)
