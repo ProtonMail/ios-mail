7b403a161bb12d1831af725e8fde0e551aa081a9
Mateusz Szklarek <mateusz.szklarek@proton.ch>
2024-10-12 09:50:51 +0200

    Refactor `HomeScreen.ModalState` usage and add tests

---
+++ Modules/App/Tests/Tests/Unit/UI/Screens/Home/HomeScreenModalFactoryTests.swift
@@ -0,0 +1,48 @@
+// Copyright (c) 2024 Proton Technologies AG
+//
+// This file is part of Proton Mail.
+//
+// Proton Mail is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Proton Mail is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Proton Mail. If not, see https://www.gnu.org/licenses/.
+
+@testable import ProtonMail
+import SwiftUI
+import ProtonContacts
+import ViewInspector
+import XCTest
+
+final class HomeScreenModalFactoryTests: XCTestCase {
+    func testMakeModal_ForContactsState_ItReturnsContactsScreen() async throws {
+        let inspect = try await modal(for: .contacts)
+
+        XCTAssertNoThrow(try inspect.find(ContactsScreen.self))
+    }
+
+    func testMakeModal_ForLabelOrFolderCreationState_ItReturnsLabelOrFolderCreationScreenScreen() async throws {
+        let inspect = try await modal(for: .labelOrFolderCreation)
+
+        XCTAssertNoThrow(try inspect.find(CreateFolderOrLabelScreen.self))
+    }
+
+    func testMakeModal_ForSettingsState_ItReturnsSettingsScreen() async throws {
+        let inspect = try await modal(for: .settings)
+
+        XCTAssertNoThrow(try inspect.find(SettingsScreen.self))
+    }
+
+    // MARK: - Private
+
+    private func modal(for state: HomeScreen.ModalState) async throws -> InspectableView<ViewType.ClassifiedView> {
+        try await HomeScreenModalFactory.makeModal(for: state).inspect()
+    }
+}
