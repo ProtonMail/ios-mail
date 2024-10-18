d2ee3f0d296278e4eab1a8d1eb07cdfe8c0a284c
Mateusz Szklarek <mateusz.szklarek@proton.ch>
2024-10-18 10:31:05 +0200

    Clean up project after creating internal modules - e.g. ProtonCore

--- Modules/App/Tests/Tests/Snapshots/Actions/MailboxItemActionSheetSnapshotTests.swift
+++ Modules/App/Tests/Tests/Snapshots/Actions/MailboxItemActionSheetSnapshotTests.swift
@@ -16,6 +16,7 @@
 // along with Proton Mail. If not, see https://www.gnu.org/licenses/.
 
 @testable import ProtonMail
+import ProtonSnapshotTesting
 
 class MailboxItemActionSheetSnapshotTests: BaseTestCase {
 
--- Modules/App/Tests/Tests/Snapshots/Toasts/ToastSceneViewSnapshotTests.swift
+++ Modules/App/Tests/Tests/Snapshots/Toasts/ToastSceneViewSnapshotTests.swift
@@ -16,6 +16,7 @@
 // along with Proton Mail. If not, see https://www.gnu.org/licenses/.
 
 @testable import ProtonMail
+import ProtonSnapshotTesting
 import XCTest
 
 class ToastSceneViewSnapshotTests: BaseTestCase {
