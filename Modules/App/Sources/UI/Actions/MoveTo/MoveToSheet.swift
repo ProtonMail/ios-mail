27b0c529edab9b3a2d34f1655ff6d1030c7f9ef9
Maciej Gomółka <maciej.gomolka@proton.ch>
2024-10-18 09:19:02 +0200

    Sort imports

--- Modules/App/Sources/UI/Actions/MoveTo/MoveToSheet.swift
+++ Modules/App/Sources/UI/Actions/MoveTo/MoveToSheet.swift
@@ -16,8 +16,8 @@
 // along with Proton Mail. If not, see https://www.gnu.org/licenses/.
 
 import DesignSystem
-import SwiftUI
 import proton_app_uniffi
+import SwiftUI
 
 struct MoveToSheet: View {
     @StateObject var model: MoveToSheetModel
