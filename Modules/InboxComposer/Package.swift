d46da55081d87639b6d8f105cf6c3ccfd808c17e
Xavi Gil <xavi.gil@proton.ch>
2024-10-30 10:55:13 +0000

    ET-1420: InboxComposer module

---
+++ Modules/InboxComposer/Package.swift
@@ -0,0 +1,36 @@
+// swift-tools-version: 5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "InboxComposer",
+    defaultLocalization: "en",
+    platforms: [.iOS(.v17)],
+    products: [
+        .library(name: "InboxComposer", targets: ["InboxComposer"]),
+    ],
+    dependencies: [
+        .package(path: "../InboxCore"),
+        .package(path: "../InboxCoreUI"),
+        .package(path: "../InboxDesignSystem"),
+        .package(path: "../InboxTesting"),
+        .package(path: "../../ProtonPackages/proton_app_uniffi")
+    ],
+    targets: [
+        .target(
+            name: "InboxComposer",
+            dependencies: ["InboxCore", "InboxCoreUI", "InboxDesignSystem", "proton_app_uniffi"],
+            resources: [
+                .process("Resources")
+            ]
+        ),
+        .testTarget(
+            name: "InboxComposerTests",
+            dependencies: [
+                .target(name: "InboxComposer"),
+                .product(name: "InboxTesting", package: "InboxTesting")
+            ]
+        )
+    ]
+)
