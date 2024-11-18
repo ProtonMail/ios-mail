b0660c014e1fe2641bfe5dc77dd98364c46a3777
Mateusz Szklarek <mateusz.szklarek@proton.ch>
2024-11-18 14:29:44 +0100

    Refactor tests in `RouteTests.swift` after self code review

--- Modules/InboxContacts/Tests/Unit/Contacts/RouteTests.swift
+++ Modules/InboxContacts/Tests/Unit/Contacts/RouteTests.swift
@@ -16,21 +16,30 @@
 // along with Proton Mail. If not, see https://www.gnu.org/licenses/.
 
 @testable import InboxContacts
+import proton_app_uniffi
 import SwiftUI
 import ViewInspector
 import XCTest
 
 final class RouteTests: XCTestCase {
     func testView_ForContactDetailsRoute_ItReturnsContactDetailsScreen() async throws {
-        let view = try await makeView(for: .contactDetails(id: .init(value: 1)))
+        let id = Id(value: 1)
+        let view = try await makeView(for: .contactDetails(id: id))
 
-        XCTAssertNoThrow(try view.find(ContactDetailsScreen.self))
+        let inspectableScreen = try view.find(ContactDetailsScreen.self)
+        let screen = try inspectableScreen.actualView()
+
+        XCTAssertEqual(screen.id, id)
     }
 
     func testView_ForContactGroupDetailsRoute_ItReturnsContactDetailsScreen() async throws {
-        let view = try await makeView(for: .contactGroupDetails(id: .init(value: 2)))
+        let id = Id(value: 2)
+        let view = try await makeView(for: .contactGroupDetails(id: id))
 
-        XCTAssertNoThrow(try view.find(ContactGroupDetailsScreen.self))
+        let inspectableScreen = try view.find(ContactGroupDetailsScreen.self)
+        let screen = try inspectableScreen.actualView()
+
+        XCTAssertEqual(screen.id, id)
     }
 
     // MARK: - Private
