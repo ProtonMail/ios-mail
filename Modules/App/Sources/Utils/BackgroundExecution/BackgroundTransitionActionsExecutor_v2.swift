da5ba55c41d8c594d9cb93f3b61203731c64f434
Maciej Gomółka <maciej.gomolka@proton.ch>
2025-02-26 17:41:12 +0100

    Duplicate temporary BackgroundTransitionActionsExecutor

---
+++ Modules/App/Sources/Utils/BackgroundExecution/BackgroundTransitionActionsExecutor_v2.swift
@@ -0,0 +1,50 @@
+// Copyright (c) 2025 Proton Technologies AG
+//
+// This file is part of Proton Mail.
+//
+// Proton Mail is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Proton Mail is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Proton Mail. If not, see https://www.gnu.org/licenses/.
+
+import proton_app_uniffi
+import UIKit
+
+class BackgroundTransitionActionsExecutor_v2: ApplicationServiceDidEnterBackground, @unchecked Sendable {
+
+    static let taskName = "finish_pending_actions"
+    private let userSession: () -> MailUserSessionProtocol?
+    private let backgroundTransitionTaskScheduler: BackgroundTransitionTaskScheduler
+
+    init(
+        userSession: @escaping () -> MailUserSessionProtocol?,
+        backgroundTransitionTaskScheduler: BackgroundTransitionTaskScheduler
+    ) {
+        self.userSession = userSession
+        self.backgroundTransitionTaskScheduler = backgroundTransitionTaskScheduler
+    }
+
+    func enterBackgroundService() {
+        guard let session = userSession() else {
+            return
+        }
+
+        let backgroundTask = backgroundTransitionTaskScheduler.beginBackgroundTask(
+            withName: Self.taskName,
+            expirationHandler: nil
+        )
+        Task {
+            _ = await session.executePendingActions()
+            backgroundTransitionTaskScheduler.endBackgroundTask(backgroundTask)
+        }
+    }
+
+}
