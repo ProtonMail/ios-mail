1c6bb4165e6467cdea4075afe6eecc24e88e369b
Maciej Gomółka <maciej.gomolka@proton.ch>
2025-03-28 15:13:18 +0100

    Move code to separate files

---
+++ Modules/App/Sources/UI/Screens/AppSettings/AppSettingsStateStore.swift
@@ -0,0 +1,84 @@
+// Copyright (c) 2025 Proton Technologies AG
+//
+// This file is part of Proton Mail.
+//
+// Proton Mail is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Proton Mail is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Proton Mail. If not, see https://www.gnu.org/licenses/.
+
+import InboxCore
+import SwiftUI
+
+final class AppSettingsStateStore: StateStore, Sendable {
+    @Published var state: AppSettingsState
+    private let notificationCenter: UserNotificationCenter
+    private let urlOpener: URLOpener
+
+    @MainActor
+    init(
+        state: AppSettingsState,
+        notificationCenter: UserNotificationCenter = UNUserNotificationCenter.current(),
+        urlOpener: URLOpener = UIApplication.shared
+    ) {
+        self.state = state
+        self.notificationCenter = notificationCenter
+        self.urlOpener = urlOpener
+    }
+
+    @MainActor
+    func handle(action: AppSettingsAction) async {
+        switch action {
+        case .notificationButtonTapped:
+            await handleNotificationsFlow()
+        case .onAppear:
+            await refreshDeviceSettings()
+        }
+    }
+
+    // MARK: - Private
+
+    @MainActor
+    private func refreshDeviceSettings() async {
+        let areNotificationsEnabled = await areNotificationsEnabled()
+        state = state.copy(\.areNotificationsEnabled, to: areNotificationsEnabled)
+    }
+
+    private func requestNotificationAuthorization() async {
+        do {
+            _ = try await notificationCenter.requestAuthorization(options: [.alert, .badge, .sound])
+        } catch {
+            AppLogger.log(error: error, category: .notifications)
+        }
+    }
+
+    @MainActor
+    private func handleNotificationsFlow() async {
+        if await notificationCenter.authorizationStatus() == .notDetermined {
+            await requestNotificationAuthorization()
+            await refreshDeviceSettings()
+        } else {
+            await urlOpener.open(.settings, options: [:])
+        }
+    }
+
+    private func areNotificationsEnabled() async -> Bool {
+        switch await notificationCenter.authorizationStatus() {
+        case .authorized:
+            true
+        case .denied, .ephemeral, .notDetermined, .provisional:
+            false
+        @unknown default:
+            fatalError()
+        }
+    }
+
+}
