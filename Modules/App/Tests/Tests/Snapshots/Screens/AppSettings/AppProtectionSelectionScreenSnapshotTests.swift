312a1262526da1861b9434c775dcbe256f010c9c
Maciej Gomółka <maciej.gomolka@proton.ch>
2025-04-16 09:40:23 +0200

    Add snapshot test for app protection selection

---
+++ Modules/App/Tests/Tests/Snapshots/Screens/AppSettings/AppProtectionSelectionScreenSnapshotTests.swift
@@ -0,0 +1,38 @@
+// Copyright (c) 2025 Proton Technologies AG
+//
+// This file is part of Proton Mail.
+//
+// Proton Mail is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Proton Mail is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Proton Mail. If not, see https://www.gnu.org/licenses/.
+
+@testable import ProtonMail
+import InboxTesting
+import InboxSnapshotTesting
+import SwiftUI
+
+class AppProtectionSelectionScreenSnapshotTests: BaseTestCase {
+
+    func testAppProtectionSelectionScreenLayoutsCorrectly() {
+        let sut = AppProtectionSelectionScreen(state: .init(
+            selectedAppProtection: .biometrics,
+            availableAppProtectionMethods: [
+                .init(type: .none, isSelected: false),
+                .init(type: .pin, isSelected: false),
+                .init(type: .faceID, isSelected: true),
+            ])
+        )
+
+        assertSnapshotsOnIPhoneX(of: sut)
+    }
+
+}
