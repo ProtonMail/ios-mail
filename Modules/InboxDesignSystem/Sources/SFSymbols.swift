d72467b765b5928029d8952bd21e1eef533c359c
Mateusz Szklarek <mateusz.szklarek@proton.ch>
2025-04-23 13:29:09 +0200

    Add missing license header for all files in InboxDesignSystem package

--- Modules/InboxDesignSystem/Sources/SFSymbols.swift
+++ Modules/InboxDesignSystem/Sources/SFSymbols.swift
@@ -1,9 +1,19 @@
+// Copyright (c) 2025 Proton Technologies AG
 //
-//  SFSymbols.swift
-//  InboxDesignSystem
+// This file is part of Proton Mail.
 //
-//  Created by Maciej Gomółka on 21.03.2025.
+// Proton Mail is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
 //
+// Proton Mail is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Proton Mail. If not, see https://www.gnu.org/licenses/.
 
 public extension DS.SFSymbols {
     static let arrowUpRightSquare = "arrow.up.right.square"
