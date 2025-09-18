4b2258fc284aa416eee9ee72eee18e1ae1928695
Jacek Krasiukianis <jacek.krasiukianis@proton.ch>
2025-09-18 12:13:24 +0200

    Fix wording

--- Modules/App/Sources/Utils/L10n.swift
+++ Modules/App/Sources/Utils/L10n.swift
@@ -478,7 +478,7 @@ enum L10n {
                 comment: "Title of the first onboarding page introducing Proton Mail's privacy focus."
             )
             static let subtitle = LocalizedStringResource(
-                "Enjoy encrypted messages, fewer spam, protection from phishing, and blocked trackers. Welcome to privacy.",
+                "Enjoy encrypted messages, less spam, protection from phishing, and blocked trackers. Welcome to privacy.",
                 comment: "Subtitle of the first onboarding page explaining Proton Mail's benefits."
             )
         }
@@ -642,7 +642,7 @@ enum L10n {
                 .init(
                     "Empty \(folderName) Folder",
                     comment: """
-                        Empty Spam/Trash Banner: Title for the confirmation alert when the user 
+                        Empty Spam/Trash Banner: Title for the confirmation alert when the user
                         is about to empty the specified location (e.g., Spam or Trash).
                         """
                 )
@@ -652,7 +652,7 @@ enum L10n {
                 .init(
                     "Are you sure you want to permanently delete all messages in the \(folderName) folder? This action cannot be undone.",
                     comment: """
-                        Empty Spam/Trash Banner: Message for the confirmation alert asking the user to confirm 
+                        Empty Spam/Trash Banner: Message for the confirmation alert asking the user to confirm
                         permanent deletion of all messages in the specified location (e.g., Spam or Trash).
                         """
                 )
@@ -888,7 +888,7 @@ enum L10n {
     enum Settings {
         enum App {
             static let title = LocalizedStringResource(
-                "App customisations",
+                "App customizations",
                 comment: "Title of the App settings screen."
             )
             static let deviceSectionTitle = LocalizedStringResource(
