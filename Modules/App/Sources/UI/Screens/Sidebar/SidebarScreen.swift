3d783f047a9e19b97280cab02ab0dcdd6c073d3f
Mateusz Szklarek <mateusz.szklarek@proton.ch>
2025-10-08 11:19:49 +0200

    Decrease `openCloseSidebarMinimumDistance` to 50 points

--- Modules/App/Sources/UI/Screens/Sidebar/SidebarScreen.swift
+++ Modules/App/Sources/UI/Screens/Sidebar/SidebarScreen.swift
@@ -32,7 +32,7 @@ struct SidebarScreen: View {
     @State private var lockedAxis: AxisLock = .none
 
     private let widthOfDragableSpaceOnTheMailbox: CGFloat = 25
-    private let openCloseSidebarMinimumDistance: CGFloat = 100
+    private let openCloseSidebarMinimumDistance: CGFloat = 30
     /// This value is to make sure a twitch of a finger when releasing the sidebar won't cause the sidebar to move in the other direction against user's wishes.
     private let lastSwipeSignificanceThreshold: CGFloat = 25
     private let animationDuration = 0.2
