575069460fac2f865900d7e12aeb15d71899809f
Mateusz Szklarek <mateusz.szklarek@proton.ch>
2025-10-17 15:02:25 +0200

    Fix `AppSettingsStateStoreTests` after conflicts resolution

--- Modules/App/Tests/Tests/Unit/UI/Screens/AppSettings/AppSettingsStateStoreTests.swift
+++ Modules/App/Tests/Tests/Unit/UI/Screens/AppSettings/AppSettingsStateStoreTests.swift
@@ -21,250 +21,218 @@ import proton_app_uniffi
 import SwiftUI
 import Testing
 
-//@MainActor
-//class AppSettingsStateStoreTests {
-//<<<<<<< HEAD
-//    private lazy var sut = AppSettingsStateStore(
-//        state: .initial,
-//        appSettingsRepository: appSettingsRepositorySpy,
-//        customSettings: customSettingsSpy,
-//        notificationCenter: notificationCenterSpy,
-//        urlOpener: urlOpenerSpy,
-//        mainBundle: bundleStub
-//    )
-//    private let notificationCenterSpy = UserNotificationCenterSpy()
-//    private let urlOpenerSpy = URLOpenerSpy()
-//    private let bundleStub = BundleStub()
-//    private let appSettingsRepositorySpy = AppSettingsRepositorySpy()
-//    private let customSettingsSpy = CustomSettingsSpy()
-//
-//    init() {
-//        bundleStub.preferredLocalizationsStub = ["en"]
-//    }
-//
-//    // MARK: - `notificationButtonTapped` action
-//
-//=======
-//    var sut: AppSettingsStateStore!
-//    let notificationCenterSpy = UserNotificationCenterSpy()
-//    let urlOpenerSpy = URLOpenerSpy()
-//    let bundleStub = BundleStub()
-//    private let appSettingsRepositorySpy = AppSettingsRepositorySpy()
-//    private let appIconConfiguratorSpy = AppIconConfiguratorSpy()
-//
-//    init() {
-//        sut = AppSettingsStateStore(
-//            state: .initial(appIconName: .none),
-//            appSettingsRepository: appSettingsRepositorySpy,
-//            notificationCenter: notificationCenterSpy,
-//            urlOpener: urlOpenerSpy,
-//            appIconConfigurator: appIconConfiguratorSpy,
-//            mainBundle: bundleStub
-//        )
-//
-//        bundleStub.preferredLocalizationsStub = ["en"]
-//    }
-//
-//>>>>>>> main
-//    @Test
-//    func whenNotificationsButtonIsTapped_ItAskForNotificationsPermissions() async {
-//        notificationCenterSpy.stubbedAuthorizationStatus = .notDetermined
-//
-//        await sut.handle(action: .notificationButtonTapped)
-//
-//        #expect(notificationCenterSpy.requestAuthorizationInvocations == [[.alert, .badge, .sound]])
-//        #expect(urlOpenerSpy.openURLInvocations.isEmpty)
-//    }
-//
-//    @Test
-//    func whenNotificationsButtonIsTapped_ItOpensNativeSettings() async {
-//        notificationCenterSpy.stubbedAuthorizationStatus = .authorized
-//
-//        await sut.handle(action: .notificationButtonTapped)
-//
-//        #expect(notificationCenterSpy.requestAuthorizationInvocations.isEmpty)
-//        #expect(urlOpenerSpy.openURLInvocations == [.settings])
-//    }
-//
-//    // MARK: - `onAppear` action
-//
-//    @Test
-//    func whenViewAppears_ItOverrideDefaultStateValuesAndSetCorrectOnes() async {
-//        notificationCenterSpy.stubbedAuthorizationStatus = .authorized
-//        bundleStub.preferredLocalizationsStub = ["pl"]
-//        appSettingsRepositorySpy.stubbedAppSettings = .init(
-//            appearance: .lightMode,
-//            protection: .biometrics,
-//            autoLock: .minutes(15),
-//            useCombineContacts: true,
-//            useAlternativeRouting: true
-//        )
-//        customSettingsSpy.stubbedSwipeToAdjacent = .ok(true)
-//
-//        await sut.handle(action: .onAppear)
-//
-//        #expect(sut.state.areNotificationsEnabled)
-//        #expect(sut.state.appLanguage == "Polish")
-//        #expect(sut.state.storedAppSettings == appSettingsRepositorySpy.stubbedAppSettings)
-//        #expect(sut.state.isSwipeToAdjacentConversationEnabled == true)
-//    }
-//
-//    // MARK: - `appearanceTapped` action
-//
-//    @Test
-//    func whenAppearanceIsTapped_WhenAppearanceIsChnaged_ItUpdatesAppearance() async {
-//        #expect(sut.state.storedAppSettings.appearance == .system)
-//        #expect(sut.state.isAppearanceMenuShown == false)
-//
-//        await sut.handle(action: .appearanceTapped)
-//        #expect(sut.state.isAppearanceMenuShown == true)
-//
-//        await changeAppAppearance(.darkMode)
-//
-//        #expect(sut.state.storedAppSettings.appearance == .darkMode)
-//
-//        #expect(appSettingsRepositorySpy.changedAppSettingsWithDiff == [.diff(appearance: .darkMode)])
-//    }
-//
-//    // MARK: - `alternativeRoutingChanged` action
-//
-//    @Test
-//    func whenAlternativeRoutingIsDisabledAndThenEnabled_ItUpdatesStoredValues() async {
-//        #expect(sut.state.storedAppSettings.useAlternativeRouting == true)
-//
-//        await changeAlternativeRoutingValue(false)
-//
-//        #expect(
-//            appSettingsRepositorySpy.changedAppSettingsWithDiff == [
-//                .diff(useAlternativeRouting: false)
-//            ])
-//        #expect(sut.state.storedAppSettings.useAlternativeRouting == false)
-//
-//        await changeAlternativeRoutingValue(true)
-//
-//        #expect(
-//            appSettingsRepositorySpy.changedAppSettingsWithDiff == [
-//                .diff(useAlternativeRouting: false),
-//                .diff(useAlternativeRouting: true),
-//            ])
-//        #expect(sut.state.storedAppSettings.useAlternativeRouting == true)
-//    }
-//
-//    @Test
-//    func whenCombinedContactsAreEnabledAndThenDisabled_ItUpdatesStoredValues() async {
-//        #expect(sut.state.storedAppSettings.useCombineContacts == false)
-//
-//        await changeCombinedContactsValue(true)
-//
-//        #expect(
-//            appSettingsRepositorySpy.changedAppSettingsWithDiff == [
-//                .diff(useCombineContacts: true)
-//            ])
-//        #expect(sut.state.storedAppSettings.useCombineContacts == true)
-//
-//        await changeCombinedContactsValue(false)
-//
-//        #expect(
-//            appSettingsRepositorySpy.changedAppSettingsWithDiff == [
-//                .diff(useCombineContacts: true),
-//                .diff(useCombineContacts: false),
-//            ])
-//        #expect(sut.state.storedAppSettings.useCombineContacts == false)
-//    }
-//
-//<<<<<<< HEAD
-//    // MARK: - `swipeToAdjacentConversationChanged` action
-//
-//    @Test
-//    func whenSwipeToAdjacentConversationChanged_AndSucceeds_ItUpdatesToGivenState() async {
-//        #expect(sut.state.isSwipeToAdjacentConversationEnabled == false)
-//
-//        await sut.handle(action: .swipeToAdjacentConversationChanged(true))
-//
-//        #expect(sut.state.isSwipeToAdjacentConversationEnabled == true)
-//
-//        await sut.handle(action: .swipeToAdjacentConversationChanged(false))
-//
-//        #expect(sut.state.isSwipeToAdjacentConversationEnabled == false)
-//    }
-//
-//    @Test
-//    func whenSwipeToAdjacentConversationChanged_AndFails_ItRevertsToPreviousState() async {
-//        #expect(sut.state.isSwipeToAdjacentConversationEnabled == false)
-//
-//        customSettingsSpy.stubbedSwipeToAdjacent = .error(ProtonError.network)
-//
-//        await sut.handle(action: .swipeToAdjacentConversationChanged(true))
-//
-//        #expect(sut.state.isSwipeToAdjacentConversationEnabled == false)
-//=======
-//    // MARK: - appIconSelected action
-//
-//    @Test
-//    func changeIconFromDefaultToCalculator() async throws {
-//        setUpSUT(with: .default)
-//
-//        await sut.handle(action: .appIconSelected(.calculator))
-//
-//        #expect(sut.state == AppSettingsState.initial(appIconName: AppIcon.calculator.alternateIconName))
-//        #expect(appIconConfiguratorSpy.setAlternateIconNameCalls == [AppIcon.calculator.alternateIconName])
-//    }
-//
-//    @Test
-//    func changeIconFromCalculatorToNotes() async throws {
-//        setUpSUT(with: .calculator)
-//
-//        await sut.handle(action: .appIconSelected(.notes))
-//
-//        #expect(sut.state == AppSettingsState.initial(appIconName: AppIcon.notes.alternateIconName))
-//        #expect(appIconConfiguratorSpy.setAlternateIconNameCalls == [AppIcon.notes.alternateIconName])
-//    }
-//
-//    @Test
-//    func changeIconFromCalculatorToDefault() async throws {
-//        setUpSUT(with: .calculator)
-//
-//        await sut.handle(action: .appIconSelected(.default))
-//
-//        #expect(sut.state == AppSettingsState.initial(appIconName: AppIcon.default.alternateIconName))
-//        #expect(appIconConfiguratorSpy.setAlternateIconNameCalls == [AppIcon.default.alternateIconName])
-//>>>>>>> main
-//    }
-//
-//    // MARK: - Private
-//
-//<<<<<<< HEAD
-//=======
-//    private func setUpSUT(with appIcon: AppIcon) {
-//        sut = AppSettingsStateStore(
-//            state: AppSettingsState.initial(appIconName: appIcon.alternateIconName),
-//            appSettingsRepository: appSettingsRepositorySpy,
-//            notificationCenter: notificationCenterSpy,
-//            urlOpener: urlOpenerSpy,
-//            appIconConfigurator: appIconConfiguratorSpy,
-//            mainBundle: bundleStub
-//        )
-//    }
-//
-//>>>>>>> main
-//    private func changeAppAppearance(_ appAppearance: AppAppearance) async {
-//        appSettingsRepositorySpy.stubbedAppSettings = appSettingsRepositorySpy.stubbedAppSettings
-//            .copy(\.appearance, to: appAppearance)
-//        await sut.handle(action: .appearanceSelected(.darkMode))
-//    }
-//
-//    private func changeCombinedContactsValue(_ value: Bool) async {
-//        appSettingsRepositorySpy.stubbedAppSettings = appSettingsRepositorySpy.stubbedAppSettings
-//            .copy(\.useCombineContacts, to: value)
-//        await sut.handle(action: .combinedContactsChanged(value))
-//    }
-//
-//    private func changeAlternativeRoutingValue(_ value: Bool) async {
-//        appSettingsRepositorySpy.stubbedAppSettings = appSettingsRepositorySpy.stubbedAppSettings
-//            .copy(\.useAlternativeRouting, to: value)
-//        await sut.handle(action: .alternativeRoutingChanged(value))
-//    }
-//}
+@MainActor
+class AppSettingsStateStoreTests {
+    private lazy var sut = setUpSUT(with: .default)
+    private let notificationCenterSpy = UserNotificationCenterSpy()
+    private let urlOpenerSpy = URLOpenerSpy()
+    private let bundleStub = BundleStub()
+    private let appSettingsRepositorySpy = AppSettingsRepositorySpy()
+    private let appIconConfiguratorSpy = AppIconConfiguratorSpy()
+    private let customSettingsSpy = CustomSettingsSpy()
+
+    init() {
+        bundleStub.preferredLocalizationsStub = ["en"]
+    }
+
+    // MARK: - `notificationButtonTapped` action
+
+    @Test
+    func whenNotificationsButtonIsTapped_ItAskForNotificationsPermissions() async {
+        notificationCenterSpy.stubbedAuthorizationStatus = .notDetermined
+
+        await sut.handle(action: .notificationButtonTapped)
+
+        #expect(notificationCenterSpy.requestAuthorizationInvocations == [[.alert, .badge, .sound]])
+        #expect(urlOpenerSpy.openURLInvocations.isEmpty)
+    }
+
+    @Test
+    func whenNotificationsButtonIsTapped_ItOpensNativeSettings() async {
+        notificationCenterSpy.stubbedAuthorizationStatus = .authorized
+
+        await sut.handle(action: .notificationButtonTapped)
+
+        #expect(notificationCenterSpy.requestAuthorizationInvocations.isEmpty)
+        #expect(urlOpenerSpy.openURLInvocations == [.settings])
+    }
+
+    // MARK: - `onAppear` action
+
+    @Test
+    func whenViewAppears_ItOverrideDefaultStateValuesAndSetCorrectOnes() async {
+        notificationCenterSpy.stubbedAuthorizationStatus = .authorized
+        bundleStub.preferredLocalizationsStub = ["pl"]
+        appSettingsRepositorySpy.stubbedAppSettings = .init(
+            appearance: .lightMode,
+            protection: .biometrics,
+            autoLock: .minutes(15),
+            useCombineContacts: true,
+            useAlternativeRouting: true
+        )
+        customSettingsSpy.stubbedSwipeToAdjacent = .ok(true)
+
+        await sut.handle(action: .onAppear)
+
+        #expect(sut.state.areNotificationsEnabled)
+        #expect(sut.state.appLanguage == "Polish")
+        #expect(sut.state.storedAppSettings == appSettingsRepositorySpy.stubbedAppSettings)
+        #expect(sut.state.isSwipeToAdjacentConversationEnabled == true)
+    }
+
+    // MARK: - `appearanceTapped` action
+
+    @Test
+    func whenAppearanceIsTapped_WhenAppearanceIsChnaged_ItUpdatesAppearance() async {
+        #expect(sut.state.storedAppSettings.appearance == .system)
+        #expect(sut.state.isAppearanceMenuShown == false)
+
+        await sut.handle(action: .appearanceTapped)
+        #expect(sut.state.isAppearanceMenuShown == true)
+
+        await changeAppAppearance(.darkMode)
+
+        #expect(sut.state.storedAppSettings.appearance == .darkMode)
+
+        #expect(appSettingsRepositorySpy.changedAppSettingsWithDiff == [.diff(appearance: .darkMode)])
+    }
+
+    // MARK: - `alternativeRoutingChanged` action
+
+    @Test
+    func whenAlternativeRoutingIsDisabledAndThenEnabled_ItUpdatesStoredValues() async {
+        #expect(sut.state.storedAppSettings.useAlternativeRouting == true)
+
+        await changeAlternativeRoutingValue(false)
+
+        #expect(
+            appSettingsRepositorySpy.changedAppSettingsWithDiff == [
+                .diff(useAlternativeRouting: false)
+            ])
+        #expect(sut.state.storedAppSettings.useAlternativeRouting == false)
+
+        await changeAlternativeRoutingValue(true)
+
+        #expect(
+            appSettingsRepositorySpy.changedAppSettingsWithDiff == [
+                .diff(useAlternativeRouting: false),
+                .diff(useAlternativeRouting: true),
+            ])
+        #expect(sut.state.storedAppSettings.useAlternativeRouting == true)
+    }
+
+    @Test
+    func whenCombinedContactsAreEnabledAndThenDisabled_ItUpdatesStoredValues() async {
+        #expect(sut.state.storedAppSettings.useCombineContacts == false)
+
+        await changeCombinedContactsValue(true)
+
+        #expect(
+            appSettingsRepositorySpy.changedAppSettingsWithDiff == [
+                .diff(useCombineContacts: true)
+            ])
+        #expect(sut.state.storedAppSettings.useCombineContacts == true)
+
+        await changeCombinedContactsValue(false)
+
+        #expect(
+            appSettingsRepositorySpy.changedAppSettingsWithDiff == [
+                .diff(useCombineContacts: true),
+                .diff(useCombineContacts: false),
+            ])
+        #expect(sut.state.storedAppSettings.useCombineContacts == false)
+    }
+
+    // MARK: - `swipeToAdjacentConversationChanged` action
+
+    @Test
+    func whenSwipeToAdjacentConversationChanged_AndSucceeds_ItUpdatesToGivenState() async {
+        #expect(sut.state.isSwipeToAdjacentConversationEnabled == false)
+
+        await sut.handle(action: .swipeToAdjacentConversationChanged(true))
+
+        #expect(sut.state.isSwipeToAdjacentConversationEnabled == true)
+
+        await sut.handle(action: .swipeToAdjacentConversationChanged(false))
+
+        #expect(sut.state.isSwipeToAdjacentConversationEnabled == false)
+    }
+
+    @Test
+    func whenSwipeToAdjacentConversationChanged_AndFails_ItRevertsToPreviousState() async {
+        #expect(sut.state.isSwipeToAdjacentConversationEnabled == false)
+
+        customSettingsSpy.stubbedSwipeToAdjacent = .error(ProtonError.network)
+
+        await sut.handle(action: .swipeToAdjacentConversationChanged(true))
+
+        #expect(sut.state.isSwipeToAdjacentConversationEnabled == false)
+    }
+
+    // MARK: - appIconSelected action
+
+    @Test
+    func changeIconFromDefaultToCalculator() async throws {
+        sut = setUpSUT(with: .default)
+
+        await sut.handle(action: .appIconSelected(.calculator))
+
+        #expect(sut.state == AppSettingsState.initial(appIconName: AppIcon.calculator.alternateIconName))
+        #expect(appIconConfiguratorSpy.setAlternateIconNameCalls == [AppIcon.calculator.alternateIconName])
+    }
+
+    @Test
+    func changeIconFromCalculatorToNotes() async throws {
+        sut = setUpSUT(with: .calculator)
+
+        await sut.handle(action: .appIconSelected(.notes))
+
+        #expect(sut.state == AppSettingsState.initial(appIconName: AppIcon.notes.alternateIconName))
+        #expect(appIconConfiguratorSpy.setAlternateIconNameCalls == [AppIcon.notes.alternateIconName])
+    }
+
+    @Test
+    func changeIconFromCalculatorToDefault() async throws {
+        sut = setUpSUT(with: .calculator)
+
+        await sut.handle(action: .appIconSelected(.default))
+
+        #expect(sut.state == AppSettingsState.initial(appIconName: AppIcon.default.alternateIconName))
+        #expect(appIconConfiguratorSpy.setAlternateIconNameCalls == [AppIcon.default.alternateIconName])
+    }
+
+    // MARK: - Private
+
+    private func changeAppAppearance(_ appAppearance: AppAppearance) async {
+        appSettingsRepositorySpy.stubbedAppSettings = appSettingsRepositorySpy.stubbedAppSettings
+            .copy(\.appearance, to: appAppearance)
+        await sut.handle(action: .appearanceSelected(.darkMode))
+    }
+
+    private func changeCombinedContactsValue(_ value: Bool) async {
+        appSettingsRepositorySpy.stubbedAppSettings = appSettingsRepositorySpy.stubbedAppSettings
+            .copy(\.useCombineContacts, to: value)
+        await sut.handle(action: .combinedContactsChanged(value))
+    }
+
+    private func changeAlternativeRoutingValue(_ value: Bool) async {
+        appSettingsRepositorySpy.stubbedAppSettings = appSettingsRepositorySpy.stubbedAppSettings
+            .copy(\.useAlternativeRouting, to: value)
+        await sut.handle(action: .alternativeRoutingChanged(value))
+    }
+
+    private func setUpSUT(with appIcon: AppIcon) -> AppSettingsStateStore {
+        AppSettingsStateStore(
+            state: AppSettingsState.initial(appIconName: appIcon.alternateIconName),
+            appSettingsRepository: appSettingsRepositorySpy,
+            customSettings: customSettingsSpy,
+            notificationCenter: notificationCenterSpy,
+            urlOpener: urlOpenerSpy,
+            appIconConfigurator: appIconConfiguratorSpy,
+            mainBundle: bundleStub
+        )
+    }
+}
 
 extension AppSettings: @retroactive Copying {}
 
