b443cd3c9cecd4d81acd8b54d09b2b755b426a11
Maciej Gomółka <maciej.gomolka@proton.ch>
2026-05-05 23:03:28 +0200

    Add snapshot tests

---
+++ Modules/App/Tests/Tests/Snapshots/UI/MailboxBars/MailboxLegacyBottomBarSnapshotTests.swift
@@ -0,0 +1,50 @@
+// Copyright (c) 2026 Proton Technologies AG
+//
+// This file is part of Proton Mail.
+//
+// Proton Mail is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Proton Mail is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Proton Mail. If not, see https://www.gnu.org/licenses/.
+
+@testable import ProtonMail
+import InboxSnapshotTesting
+import SwiftUI
+import Testing
+
+@MainActor
+struct MailboxLegacyBottomBarSnapshotTests {
+    struct TestCase: Sendable {
+        let state: UnreadButtonState
+        let name: String
+    }
+
+    @Test(arguments: [
+        TestCase(
+            state: .init(isSelected: false, counterState: .known(unreadCount: 5)),
+            name: "unread_deselected_known_count"
+        ),
+        TestCase(
+            state: .init(isSelected: true, counterState: .known(unreadCount: 5)),
+            name: "unread_selected_known_count"
+        ),
+        TestCase(
+            state: .init(isSelected: false, counterState: .unknown),
+            name: "unread_deselected_unknown_count"
+        ),
+    ])
+    func allStates(_ testCase: TestCase) {
+        assertSelfSizingSnapshot(
+            of: MailboxLegacyBottomBarView(state: testCase.state, action: { _ in }),
+            named: testCase.name
+        )
+    }
+}
---
+++ Modules/App/Tests/Tests/Snapshots/UI/MailboxBars/MailboxLiquidGlassTopBarSnapshotTests.swift
@@ -0,0 +1,44 @@
+// Copyright (c) 2026 Proton Technologies AG
+//
+// This file is part of Proton Mail.
+//
+// Proton Mail is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Proton Mail is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Proton Mail. If not, see https://www.gnu.org/licenses/.
+
+@testable import ProtonMail
+import InboxSnapshotTesting
+import SwiftUI
+import Testing
+
+@MainActor
+struct MailboxLiquidGlassTopBarSnapshotTests {
+    struct TestCase: Sendable {
+        let state: MailboxTopBarState
+        let name: String
+    }
+
+    @Test(arguments: [
+        TestCase(state: .selectionMode(.canSelectMoreItems), name: "selection_can_select"),
+        TestCase(state: .selectionMode(.noMoreItemsToSelect), name: "selection_no_more_items"),
+        TestCase(state: .includeSpamTrash(isSelected: false), name: "spam_trash_deselected"),
+        TestCase(state: .includeSpamTrash(isSelected: true), name: "spam_trash_selected"),
+    ])
+    @available(iOS 26, *)
+    func allStates(_ testCase: TestCase) {
+        assertSelfSizingSnapshot(
+            of: MailboxLiquidGlassTopBar(state: testCase.state, action: { _ in }),
+            drawHierarchyInKeyWindow: true,
+            named: testCase.name
+        )
+    }
+}
